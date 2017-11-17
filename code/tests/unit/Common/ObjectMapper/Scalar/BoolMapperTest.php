ef5248ca8a009282deaa43b4e95d1318318adadf
Markus Reinhold <markusreinhold@icloud.com>
2017-11-18 00:01:27 +0100

    Add unit test for BoolMapper

---
+++ code/tests/unit/Common/ObjectMapper/Scalar/BoolMapperTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gambling\Common\ObjectMapper\Scalar;
+
+use PHPUnit\Framework\TestCase;
+
+class BoolMapperTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param mixed $value
+     */
+    public function itShouldSerialize($value): void
+    {
+        $mapper = new BoolMapper();
+        $this->assertSame(
+            (bool)$value,
+            $mapper->serialize($value)
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param mixed $value
+     */
+    public function itShouldDeserialize($value): void
+    {
+        $mapper = new BoolMapper();
+        $this->assertSame(
+            (bool)$value,
+            $mapper->deserialize($value)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function valuesProvider(): array
+    {
+        return [
+            [1337], [13.37], ['1337'], ['foobar'], [true], [false], [null]
+        ];
+    }
+}
