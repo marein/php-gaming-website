49195ca7a9ef68cca0620a5a251b26cedb392513
Markus Reinhold <markusreinhold@icloud.com>
2017-11-18 00:03:23 +0100

    Add unit test for IntMapper

---
+++ code/tests/unit/Common/ObjectMapper/Scalar/IntMapperTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gambling\Common\ObjectMapper\Scalar;
+
+use PHPUnit\Framework\TestCase;
+
+class IntMapperTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param mixed $value
+     */
+    public function itShouldSerialize($value): void
+    {
+        $mapper = new IntMapper();
+        $this->assertSame(
+            (int)$value,
+            $mapper->serialize($value)
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param mixed $value
+     */
+    public function itShouldDeserialize($value): void
+    {
+        $mapper = new IntMapper();
+        $this->assertSame(
+            (int)$value,
+            $mapper->deserialize($value)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function valuesProvider(): array
+    {
+        return [
+            [1337], [13.37], ['1337'], ['foobar'], [true], [false], [null]
+        ];
+    }
+}
