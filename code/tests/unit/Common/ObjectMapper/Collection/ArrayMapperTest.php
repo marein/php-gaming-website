5d62a37f2444f4e5a46eccecdb3e5c26338c4870
Markus Reinhold <markusreinhold@icloud.com>
2017-11-18 00:13:49 +0100

    Add unit test for ArrayMapper

---
+++ code/tests/unit/Common/ObjectMapper/Collection/ArrayMapperTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Gambling\Common\ObjectMapper\Collection;
+
+use Gambling\Common\ObjectMapper\Mapper;
+use PHPUnit\Framework\TestCase;
+
+final class ArrayMapperTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldSerialize(): void
+    {
+        $expected = ['a', 'a', 'a'];
+        $innerMapper = $this->createMock(Mapper::class);
+        $innerMapper
+            ->expects($this->exactly(3))
+            ->method('serialize')
+            ->willReturn('a');
+
+        /** @var Mapper $innerMapper */
+        $arrayMapper = new ArrayMapper($innerMapper);
+        $serialized = $arrayMapper->serialize([1, 2, 3]);
+
+        $this->assertSame($expected, $serialized);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldDeserialize(): void
+    {
+        $expected = ['a', 'a', 'a'];
+        $innerMapper = $this->createMock(Mapper::class);
+        $innerMapper
+            ->expects($this->exactly(3))
+            ->method('deserialize')
+            ->willReturn('a');
+
+        /** @var Mapper $innerMapper */
+        $arrayMapper = new ArrayMapper($innerMapper);
+        $serialized = $arrayMapper->deserialize([1, 2, 3]);
+
+        $this->assertSame($expected, $serialized);
+    }
+}
