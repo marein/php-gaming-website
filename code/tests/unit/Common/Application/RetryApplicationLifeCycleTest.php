e494e608db6f5b95b24a3515e9e1ef04e2154082
Markus Reinhold <markusreinhold@icloud.com>
2017-11-17 20:02:05 +0100

    Add unit test for RetryApplicationLifeCycle

---
+++ code/tests/unit/Common/Application/RetryApplicationLifeCycleTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Gambling\Common\Application;
+
+use PHPUnit\Framework\TestCase;
+
+class RetryApplicationLifeCycleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldThrowAnExceptionIfNumberOfRetriesIsLowerThanOne(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        /** @var ApplicationLifeCycle $applicationLifeCycle */
+        $applicationLifeCycle = $this->createMock(ApplicationLifeCycle::class);
+
+        new RetryApplicationLifeCycle(
+            $applicationLifeCycle,
+            0
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldRetry(): void
+    {
+        $applicationLifeCycle = $this->createMock(ApplicationLifeCycle::class);
+
+        // The first two times when "run" is called, an exception is thrown.
+        $applicationLifeCycle->expects($this->at(0))->method('run')->willThrowException(
+            new \Exception()
+        );
+        $applicationLifeCycle->expects($this->at(1))->method('run')->willThrowException(
+            new \Exception()
+        );
+
+        // Expect that "run" is called three times.
+        $applicationLifeCycle->expects($this->exactly(3))->method('run');
+
+        /** @var ApplicationLifeCycle $applicationLifeCycle */
+        $retryApplicationLifeCycle = new RetryApplicationLifeCycle(
+            $applicationLifeCycle,
+            3
+        );
+
+        $retryApplicationLifeCycle->run(function () {
+            // No op
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldThrowTheApplicationExceptionWhenNumberOfRetriesAreReached(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Custom exception');
+
+        $applicationLifeCycle = $this->createMock(ApplicationLifeCycle::class);
+
+        // "run" always throws an exception.
+        $applicationLifeCycle->expects($this->any())->method('run')->willThrowException(
+            new \Exception('Custom exception')
+        );
+
+        // Expect that "run" is called three times.
+        $applicationLifeCycle->expects($this->exactly(3))->method('run');
+
+        /** @var ApplicationLifeCycle $applicationLifeCycle */
+        $retryApplicationLifeCycle = new RetryApplicationLifeCycle(
+            $applicationLifeCycle,
+            3
+        );
+
+        $retryApplicationLifeCycle->run(function () {
+            // No op
+        });
+    }
+}
