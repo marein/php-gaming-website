63b3f26810cbcd3cc30d76261818076ecc1ebae1
Markus Reinhold <markusreinhold@icloud.com>
2017-11-18 00:21:34 +0100

    Add unit test for SplFixedArrayMapper

---
+++ code/tests/unit/Common/ObjectMapper/Collection/SplFixedArrayMapperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Gambling\Common\ObjectMapper\Collection;
+
+use Gambling\Common\ObjectMapper\Mapper;
+use PHPUnit\Framework\TestCase;
+
+final class SplFixedArrayMapperTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldSerialize(): void
+    {
+        $expected = ['a', 'a', 'a'];
+        $innerMapper = $this->createMock(Mapper::class);
+        $innerMapper
+            ->expects($this->exactly(3))
+            ->method('serialize')
+            ->willReturn('a');
+
+        /** @var Mapper $innerMapper */
+        $arrayMapper = new SplFixedArrayMapper($innerMapper);
+        $serialized = $arrayMapper->serialize(
+            \SplFixedArray::fromArray([1, 2, 3])
+        );
+
+        $this->assertSame($expected, $serialized);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldDeserialize(): void
+    {
+        $expected = \SplFixedArray::fromArray(['a', 'a', 'a']);
+        $innerMapper = $this->createMock(Mapper::class);
+        $innerMapper
+            ->expects($this->exactly(3))
+            ->method('deserialize')
+            ->willReturn('a');
+
+        /** @var Mapper $innerMapper */
+        $arrayMapper = new SplFixedArrayMapper($innerMapper);
+        $serialized = $arrayMapper->deserialize([1, 2, 3]);
+
+        $this->assertEquals($expected, $serialized);
+    }
+}
