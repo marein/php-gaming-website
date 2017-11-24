0c0522bfa8ca3b1dc6a66669a6cfb7f481e7d33a
Markus Reinhold <markusreinhold@icloud.com>
2017-11-24 22:36:17 +0100

    Add DoctrineReconnectTransactionalBus

---
+++ code/src/Common/Port/Adapter/Bus/DoctrineReconnectTransactionalBus.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Gambling\Common\Port\Adapter\Bus;
+
+use Doctrine\DBAL\Connection;
+use Gambling\Common\Bus\Bus;
+
+/**
+ * This class aims to get around the "Server has gone away" Exception.
+ * It saves the time of the last handle. If the current handle reached the maximum number of idle between handles,
+ * the connection gets reconnected. So, the value should be lower than the timeout in your database.
+ *
+ * IMPORTANT: This class should wrap any transactional behaviour, not the other way around.
+ */
+final class DoctrineReconnectTransactionalBus implements Bus
+{
+    /**
+     * @var Bus
+     */
+    private $bus;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var int
+     */
+    private $idleBetweenHandlesInSeconds;
+
+    /**
+     * @var int
+     */
+    private $timeOfLastHandle;
+
+    /**
+     * DoctrineReconnectTransactionalBus constructor.
+     *
+     * @param Bus        $bus
+     * @param Connection $connection
+     * @param int        $idleBetweenHandlesInSeconds
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(
+        Bus $bus,
+        Connection $connection,
+        int $idleBetweenHandlesInSeconds
+    ) {
+        if ($idleBetweenHandlesInSeconds < 1) {
+            throw new \InvalidArgumentException(
+                'Idle between handles in seconds must be greater than 0.'
+            );
+        }
+
+        $this->bus = $bus;
+        $this->connection = $connection;
+        $this->idleBetweenHandlesInSeconds = $idleBetweenHandlesInSeconds;
+        $this->timeOfLastHandle = time();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function handle($command)
+    {
+        $this->reconnectIfTimedOut();
+
+        $return = $this->bus->handle($command);
+
+        $this->timeOfLastHandle = time();
+
+        return $return;
+    }
+
+    /**
+     * Reconnecting the connection if the last handle is too long ago.
+     */
+    private function reconnectIfTimedOut(): void
+    {
+        if (time() - $this->timeOfLastHandle > $this->idleBetweenHandlesInSeconds) {
+            echo 'Reconnect...' . PHP_EOL;
+            $this->connection->close();
+            $this->connection->connect();
+        }
+    }
+}
