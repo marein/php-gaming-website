ba0e96103d8943330ab6bf166d3642c096ce2a1b
Markus Reinhold <markusreinhold@icloud.com>
2017-11-29 19:48:48 +0100

    Add unit test for Field

---
+++ code/tests/unit/ConnectFour/Domain/Game/Board/FieldTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game\Board;
+
+use PHPUnit\Framework\TestCase;
+
+class FieldTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedEmpty(): void
+    {
+        $field = Field::empty(new Point(0, 1));
+
+        $this->assertTrue($field->isEmpty());
+        $this->assertEquals(Stone::none(), $field->stone());
+    }
+
+    /**
+     * @test
+     */
+    public function aStoneCanBePlaced(): void
+    {
+        $field = Field::empty(new Point(0, 1));
+
+        $fieldWithStone = $field->placeStone(Stone::red());
+
+        $this->assertFalse($fieldWithStone->isEmpty());
+        $this->assertEquals(Stone::red(), $fieldWithStone->stone());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanBeTypeCastedToString(): void
+    {
+        $field = Field::empty(new Point(0, 1));
+
+        $this->assertSame((string)Stone::none()->color(), (string)$field);
+
+        $fieldWithStone = $field->placeStone(Stone::red());
+
+        $this->assertSame((string)Stone::red()->color(), (string)$fieldWithStone);
+    }
+}
