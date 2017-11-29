2540317fa273eee3e5dae2d8e539c59b86f4b673
Markus Reinhold <markusreinhold@icloud.com>
2017-11-29 19:47:59 +0100

    Add unit test for Stone

---
+++ code/tests/unit/ConnectFour/Domain/Game/Board/StoneTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game\Board;
+
+use PHPUnit\Framework\TestCase;
+
+class StoneTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function aRedStoneCanBeCreated(): void
+    {
+        $this->assertSame(Stone::red()->color(), Stone::RED);
+    }
+
+    /**
+     * @test
+     */
+    public function aYellowStoneCanBeCreated(): void
+    {
+        $this->assertSame(Stone::yellow()->color(), Stone::YELLOW);
+    }
+
+    /**
+     * @test
+     */
+    public function aNoneStoneCanBeCreated(): void
+    {
+        $this->assertSame(Stone::none()->color(), Stone::NONE);
+    }
+}
