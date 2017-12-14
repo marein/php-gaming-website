a9b31dd92076adee7ad3b0084d3cf020bf23e0f0
Markus Reinhold <markusreinhold@icloud.com>
2017-12-14 17:18:07 +0100

    Add unit test for Player

---
+++ code/tests/unit/ConnectFour/Domain/Game/PlayerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game;
+
+use Gambling\ConnectFour\Domain\Game\Board\Stone;
+use Gambling\ConnectFour\Domain\Game\Exception\PlayerHasInvalidStoneException;
+use PHPUnit\Framework\TestCase;
+
+class PlayerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithItsValues(): void
+    {
+        $id = uniqid();
+        $stone = Stone::red();
+
+        $player = new Player($id, $stone);
+
+        $this->assertEquals($id, $player->id());
+        $this->assertEquals($stone, $player->stone());
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldNotBeCreatedWithNoneStone(): void
+    {
+        $this->expectException(PlayerHasInvalidStoneException::class);
+
+        $id = uniqid();
+        $stone = Stone::none();
+
+        new Player($id, $stone);
+    }
+}
