f054a7f9e6b92a946e3f8194b86d4359623269a6
Markus Reinhold <markusreinhold@icloud.com>
2018-01-04 23:37:18 +0100

    Replay moves in the game view #10

--- code/src/WebInterface/Presentation/Http/JavaScript/ConnectFour/Game.js
+++ code/src/WebInterface/Presentation/Http/JavaScript/ConnectFour/Game.js
@@ -7,35 +7,103 @@ Gambling.ConnectFour.Game = class
      * @param {Gambling.Common.EventPublisher} eventPublisher
      * @param {Gambling.ConnectFour.GameService} gameService
      * @param {Node} gameHolder
+     * @param {Node} previousMoveButton
+     * @param {Node} nextMoveButton
+     * @param {Node} followMovesButton
      * @param {String} gameId
      * @param {{x:Number, y:Number, color:Number}[]} moves
      */
-    constructor(eventPublisher, gameService, gameHolder, gameId, moves)
+    constructor(
+        eventPublisher,
+        gameService,
+        gameHolder,
+        previousMoveButton,
+        nextMoveButton,
+        followMovesButton,
+        gameId,
+        moves
+    )
     {
         this.eventPublisher = eventPublisher;
         this.gameService = gameService;
         this.gameHolder = gameHolder;
+        this.previousMoveButton = previousMoveButton;
+        this.nextMoveButton = nextMoveButton;
+        this.followMovesButton = followMovesButton;
         this.gameId = gameId;
         this.moves = moves;
+        this.numberOfCurrentMoveInView = this.moves.length;
         this.fields = this.gameHolder.querySelectorAll('.game__field');
         this.colorToClass = {
             1: 'game__field--red',
             2: 'game__field--yellow'
         };
 
-        this.moves.forEach(this.addMove.bind(this));
+        this.showMovesUpTo(this.numberOfCurrentMoveInView);
         this.registerEventHandler();
     }
 
     /**
+     * @param {Number} index
+     */
+    showMovesUpTo(index)
+    {
+        // Clear fields
+        this.fields.forEach((field) => {
+            field.classList.remove('game__field--red');
+            field.classList.remove('game__field--yellow');
+        });
+
+        this.moves.slice(0, index).forEach(this.showMove.bind(this));
+        this.updateNavigationButtons();
+    }
+
+    /**
+     * Display the move in the view.
+     *
      * @param {{x:Number, y:Number, color:Number}} move
      */
-    addMove(move)
+    showMove(move)
     {
         let field = this.gameHolder.querySelector('.game__field[data-point="' + move.x + ' ' + move.y + '"]');
         field.classList.add(
             this.colorToClass[move.color]
         );
+    }
+
+    /**
+     * Push the move in the list. Display the move only if the user looks at the last move.
+     *
+     * @param {{x:Number, y:Number, color:Number}} move
+     */
+    addMove(move)
+    {
+        this.moves.push(move);
+
+        // Only show if the the user follow the moves.
+        if (this.followMovesButton.disabled === true) {
+            this.showMove(move);
+            this.numberOfCurrentMoveInView++;
+            this.updateNavigationButtons();
+        }
+    }
+
+    /**
+     * Updates the previous move, next move und follow moves button according to the state of
+     * the number of the current move in view.
+     */
+    updateNavigationButtons()
+    {
+        let isCurrentMoveTheLastMove = this.numberOfCurrentMoveInView === this.moves.length;
+        let isCurrentMoveBeforeTheFirstMove = this.numberOfCurrentMoveInView === 0;
+
+        this.nextMoveButton.disabled = isCurrentMoveTheLastMove;
+        this.followMovesButton.disabled = isCurrentMoveTheLastMove;
+        this.previousMoveButton.disabled = isCurrentMoveBeforeTheFirstMove;
+
+        this.nextMoveButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
+        this.followMovesButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
+        this.previousMoveButton.classList.toggle('disabled', isCurrentMoveBeforeTheFirstMove);
     }
 
     onFieldClick(event)
@@ -60,6 +128,30 @@ Gambling.ConnectFour.Game = class
         this.addMove(event.payload);
     }
 
+    onPreviousMoveClick(event)
+    {
+        event.preventDefault();
+
+        this.numberOfCurrentMoveInView--;
+        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+    }
+
+    onNextMoveClick(event)
+    {
+        event.preventDefault();
+
+        this.numberOfCurrentMoveInView++;
+        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+    }
+
+    onFollowMovesClick(event)
+    {
+        event.preventDefault();
+
+        this.numberOfCurrentMoveInView = this.moves.length;
+        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+    }
+
     registerEventHandler()
     {
         this.eventPublisher.subscribe({
@@ -72,5 +164,9 @@ Gambling.ConnectFour.Game = class
         this.fields.forEach((field) => {
             field.addEventListener('click', this.onFieldClick.bind(this));
         });
+
+        this.previousMoveButton.addEventListener('click', this.onPreviousMoveClick.bind(this));
+        this.nextMoveButton.addEventListener('click', this.onNextMoveClick.bind(this));
+        this.followMovesButton.addEventListener('click', this.onFollowMovesClick.bind(this));
     }
 };
