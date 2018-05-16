23ba3000d4f9f39456a6d393df7c43c0cf9c8486
Markus Reinhold <markusreinhold@icloud.com>
2018-05-16 18:47:43 +0200

    Add unit test for Gambling\ConnectFour\Domain\Game\Event\ChatAssigned

---
+++ code/tests/unit/ConnectFour/Domain/Game/Event/ChatAssignedTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game\Event;
+
+use Gambling\Common\Clock\Clock;
+use Gambling\ConnectFour\Domain\Game\GameId;
+use PHPUnit\Framework\TestCase;
+
+final class ChatAssignedTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithItsValues(): void
+    {
+        Clock::instance()->freeze();
+
+        $gameId = GameId::generate();
+        $chatId = 'chatId';
+        $payload = [
+            'gameId' => $gameId->toString(),
+            'chatId' => $chatId
+        ];
+
+        $chatAssigned = new ChatAssigned($gameId, $chatId);
+
+        $this->assertSame('ChatAssigned', $chatAssigned->name());
+        $this->assertSame($gameId->toString(), $chatAssigned->aggregateId());
+        $this->assertSame(Clock::instance()->now(), $chatAssigned->occurredOn());
+        $this->assertSame($payload, $chatAssigned->payload());
+
+        Clock::instance()->resume();
+    }
+}
