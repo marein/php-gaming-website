a22e9f71b9f3a52ecc72d052010f02eb97d04961
Markus Reinhold <markusreinhold@icloud.com>
2018-05-16 18:57:28 +0200

    Add unit test for Gambling\ConnectFour\Domain\Game\Event\GameWon

---
+++ code/tests/unit/ConnectFour/Domain/Game/Event/GameWonTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game\Event;
+
+use Gambling\Common\Clock\Clock;
+use Gambling\ConnectFour\Domain\Game\Board\Stone;
+use Gambling\ConnectFour\Domain\Game\GameId;
+use Gambling\ConnectFour\Domain\Game\Player;
+use PHPUnit\Framework\TestCase;
+
+final class GameWonTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithItsValues(): void
+    {
+        Clock::instance()->freeze();
+
+        $gameId = GameId::generate();
+        $winnerPlayerId = 'winnerPlayerId';
+        $payload = [
+            'gameId'         => $gameId->toString(),
+            'winnerPlayerId' => $winnerPlayerId
+        ];
+
+        $gameWon = new GameWon(
+            $gameId,
+            new Player($winnerPlayerId, Stone::red())
+        );
+
+        $this->assertSame('GameWon', $gameWon->name());
+        $this->assertSame($gameId->toString(), $gameWon->aggregateId());
+        $this->assertSame(Clock::instance()->now(), $gameWon->occurredOn());
+        $this->assertSame($payload, $gameWon->payload());
+
+        Clock::instance()->resume();
+    }
+}
