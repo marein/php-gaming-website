d2c20b56437aab6cebe6ab7b1769969c98ea7c62
Markus Reinhold <markusreinhold@icloud.com>
2018-05-16 18:55:04 +0200

    Add unit test for Gambling\ConnectFour\Domain\Game\Event\GameOpened

---
+++ code/tests/unit/ConnectFour/Domain/Game/Event/GameOpenedTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Gambling\ConnectFour\Domain\Game\Event;
+
+use Gambling\Common\Clock\Clock;
+use Gambling\ConnectFour\Domain\Game\Board\Size;
+use Gambling\ConnectFour\Domain\Game\Board\Stone;
+use Gambling\ConnectFour\Domain\Game\GameId;
+use Gambling\ConnectFour\Domain\Game\Player;
+use PHPUnit\Framework\TestCase;
+
+final class GameOpenedTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldBeCreatedWithItsValues(): void
+    {
+        Clock::instance()->freeze();
+
+        $gameId = GameId::generate();
+        $width = 7;
+        $height = 6;
+        $playerId = 'playerId';
+        $payload = [
+            'gameId'   => $gameId->toString(),
+            'width'    => $width,
+            'height'   => $height,
+            'playerId' => $playerId
+        ];
+
+        $gameOpened = new GameOpened(
+            $gameId,
+            new Size($width, $height),
+            new Player($playerId, Stone::red())
+        );
+
+        $this->assertSame('GameOpened', $gameOpened->name());
+        $this->assertSame($gameId->toString(), $gameOpened->aggregateId());
+        $this->assertSame(Clock::instance()->now(), $gameOpened->occurredOn());
+        $this->assertSame($payload, $gameOpened->payload());
+
+        Clock::instance()->resume();
+    }
+}
