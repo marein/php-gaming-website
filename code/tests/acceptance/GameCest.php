22e381089f583fd52c29d1102c8bee0cf7d0df9e
Markus Reinhold <markusreinhold@icloud.com>
2018-09-15 12:44:16 +0200

    Write acceptance tests
    I can abort a game with a joined friend

--- code/tests/acceptance/GameCest.php
+++ code/tests/acceptance/GameCest.php
@@ -11,4 +11,27 @@ class GameCest
         $I->click('.game-list__game--user-game');
         $I->waitForElementNotVisible('[data-game-id="' . $gameId . '"]', 5);
     }
+
+    public function iCanAbortAGameWithAJoinedFriend(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+        $I->click('[data-open-game-button]');
+        $I->waitForElement('.game-list__game--user-game', 2);
+        $gameId = $I->grabAttributeFrom('.game-list__game--user-game', 'data-game-id');
+
+        $jane = $I->haveFriend('jane');
+        $jane->does(
+            function (AcceptanceTester $I) use ($gameId) {
+                $I->amOnPage('/');
+                $I->waitForElement('[data-game-id="' . $gameId . '"]', 2);
+                $I->click('[data-game-id="' . $gameId . '"]');
+                $I->waitForCurrentUrl('/game/' . $gameId, 2);
+            }
+        );
+
+        $I->waitForCurrentUrl('/game/' . $gameId, 2);
+        $I->click('#abort-game');
+
+        // todo: Add missing assertion. Currently not possible because the user interface is not updated.
+    }
 }
