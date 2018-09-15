bea9ab2205ed64765df830c0fd0bb227b5bc45a4
Markus Reinhold <markusreinhold@icloud.com>
2018-09-15 12:35:17 +0200

    Fix acceptance tests
    The test "I can open a game and abort it" acts on its own game.

--- code/tests/acceptance/GameCest.php
+++ code/tests/acceptance/GameCest.php
@@ -2,12 +2,13 @@
 
 class GameCest
 {
-    public function iCanOpenAGameAndAbortIt(AcceptanceTester $I)
+    public function iCanOpenAGameAndAbortIt(AcceptanceTester $I): void
     {
         $I->amOnPage('/');
         $I->click('[data-open-game-button]');
-        $I->waitForElement('.game-list__game', 2);
-        $I->click('.game-list__game');
-        $I->waitForElementNotVisible('.game-list__game', 5);
+        $I->waitForElement('.game-list__game--user-game', 2);
+        $gameId = $I->grabAttributeFrom('.game-list__game--user-game', 'data-game-id');
+        $I->click('.game-list__game--user-game');
+        $I->waitForElementNotVisible('[data-game-id="' . $gameId . '"]', 5);
     }
 }
