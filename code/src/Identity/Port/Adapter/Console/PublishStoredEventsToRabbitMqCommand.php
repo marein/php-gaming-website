8c277490289088d8316da7f9e2321ce4bdbe6060
Markus Reinhold <markusreinhold@icloud.com>
2018-09-22 00:29:54 +0200

    Publish stored events in Identity to RabbitMq #14

---
+++ code/src/Identity/Port/Adapter/Console/PublishStoredEventsToRabbitMqCommand.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace Gambling\Identity\Port\Adapter\Console;
+
+use Gambling\Common\EventStore\ConsistentOrderEventStore;
+use Gambling\Common\EventStore\EventStore;
+use Gambling\Common\EventStore\FollowEventStoreDispatcher;
+use Gambling\Common\EventStore\InMemoryCacheEventStorePointer;
+use Gambling\Common\EventStore\StoredEventPublisher;
+use Gambling\Common\EventStore\ThrottlingEventStore;
+use Gambling\Common\MessageBroker\MessageBroker;
+use Gambling\Common\Port\Adapter\EventStore\PredisEventStorePointer;
+use Gambling\Common\Port\Adapter\EventStore\Subscriber\SymfonyConsoleDebugSubscriber;
+use Gambling\Identity\Port\Adapter\Messaging\PublishStoredEventsToRabbitMqSubscriber;
+use Predis\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class PublishStoredEventsToRabbitMqCommand extends Command
+{
+    /**
+     * @var EventStore
+     */
+    private $eventStore;
+
+    /**
+     * @var Client
+     */
+    private $predis;
+
+    /**
+     * @var MessageBroker
+     */
+    private $messageBroker;
+
+    /**
+     * PublishStoredEventsToRabbitMqCommand constructor.
+     *
+     * @param EventStore    $eventStore
+     * @param Client        $predis
+     * @param MessageBroker $messageBroker
+     */
+    public function __construct(EventStore $eventStore, Client $predis, MessageBroker $messageBroker)
+    {
+        parent::__construct();
+
+        $this->eventStore = $eventStore;
+        $this->predis = $predis;
+        $this->messageBroker = $messageBroker;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('identity:publish-stored-events-to-rabbit-mq');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // The creation of FollowEventStoreDispatcher could be done via container.
+        $debugSubscriber = new SymfonyConsoleDebugSubscriber($output);
+        $rabbitMqSubscriber = new PublishStoredEventsToRabbitMqSubscriber(
+            $this->messageBroker
+        );
+
+        $eventStorePointer = new InMemoryCacheEventStorePointer(
+            new PredisEventStorePointer(
+                $this->predis,
+                'rabbit-mq-pointer'
+            )
+        );
+
+        $storedEventPublisher = new StoredEventPublisher();
+        $storedEventPublisher->subscribe($rabbitMqSubscriber);
+        $storedEventPublisher->subscribe($debugSubscriber);
+
+        $followEventStoreDispatcher = new FollowEventStoreDispatcher(
+            $eventStorePointer,
+            new ThrottlingEventStore(
+                new ConsistentOrderEventStore($this->eventStore),
+                100
+            ),
+            $storedEventPublisher
+        );
+
+        while (true) {
+            $followEventStoreDispatcher->dispatch(1000);
+        }
+    }
+}
---
+++ code/src/Identity/Port/Adapter/Messaging/PublishStoredEventsToRabbitMqSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Gambling\Identity\Port\Adapter\Messaging;
+
+use Gambling\Common\EventStore\StoredEvent;
+use Gambling\Common\EventStore\StoredEventSubscriber;
+use Gambling\Common\MessageBroker\MessageBroker;
+
+final class PublishStoredEventsToRabbitMqSubscriber implements StoredEventSubscriber
+{
+    /**
+     * @var MessageBroker
+     */
+    private $messageBroker;
+
+    /**
+     * PublishStoredEventsToRabbitMqSubscriber constructor.
+     *
+     * @param MessageBroker $messageBroker
+     */
+    public function __construct(MessageBroker $messageBroker)
+    {
+        $this->messageBroker = $messageBroker;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function handle(StoredEvent $storedEvent): void
+    {
+        // We should definitely filter the events we are going to publish,
+        // since that belongs to our public interface for the other contexts.
+        // However, it's not done for simplicity in this sample project.
+        // We could
+        //     * publish specific messages by name.
+        //     * filter out specific properties in the payload.
+        //     * translate when the properties for an event in the payload changed.
+        $this->messageBroker->publish(
+            $storedEvent->payload(),
+            'Identity.' . $storedEvent->name()
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isSubscribedTo(StoredEvent $storedEvent): bool
+    {
+        return true;
+    }
+}
