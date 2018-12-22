5d45bb9bbb3d49e036f8c6aa45478a42cb5ec3da
Markus Reinhold <markusreinhold@icloud.com>
2018-12-22 23:56:21 +0100

    Add ConnectFourGame component #18

--- code/web/assets/js-legacy/ConnectFour/Game.js
+++ code/web/assets/js-legacy/ConnectFour/Game.js
@@ -1,58 +1,79 @@
 import { service } from '../../js/ConnectFour/GameService.js'
 import { Game as GameModel } from '../../js/ConnectFour/Model/Game.js'
 
-window.Gaming = window.Gaming || {};
-window.Gaming.ConnectFour = window.Gaming.ConnectFour || {};
+/**
+ * todo: Render the game within <canvas>, see https://github.com/marein/php-gaming-website/issues/19.
+ */
 
-window.Gaming.ConnectFour.Game = class
+class GameElement extends HTMLElement
 {
-    /**
-     * @param {Gaming.Common.EventPublisher} eventPublisher
-     * @param {Node} gameHolder
-     * @param {Node} previousMoveButton
-     * @param {Node} nextMoveButton
-     * @param {Node} followMovesButton
-     */
-    constructor(
-        eventPublisher,
-        gameHolder,
-        previousMoveButton,
-        nextMoveButton,
-        followMovesButton
-    ) {
-        this.eventPublisher = eventPublisher;
-        this.gameHolder = gameHolder;
-        this.previousMoveButton = previousMoveButton;
-        this.nextMoveButton = nextMoveButton;
-        this.followMovesButton = followMovesButton;
-        this.game = GameModel.fromObject(
-            JSON.parse(this.gameHolder.dataset.game)
+    connectedCallback()
+    {
+        let game = JSON.parse(this.getAttribute('game'));
+
+        this._gameHolder = document.createElement('div');
+        this._gameHolder.classList.add('box');
+
+        let table = document.createElement('table');
+        table.classList.add('game');
+
+        let tbody = document.createElement('tbody');
+
+        for (let y = 1; y <= game.height; y++) {
+            let tr = document.createElement('tr');
+
+            for (let x = 1; x <= game.width; x++) {
+                let td = document.createElement('td');
+                td.classList.add('game__field');
+                td.setAttribute('data-column', x.toString());
+                td.setAttribute('data-point', x.toString() + ' ' + y.toString());
+
+                tr.append(td);
+            }
+
+            tbody.append(tr);
+        }
+
+        table.append(tbody);
+        this._gameHolder.append(table);
+        this.append(this._gameHolder);
+
+        this._previousMoveButton = document.querySelector(
+            this.getAttribute('previous-move-selector')
         );
-        this.numberOfCurrentMoveInView = this.game.numberOfMoves();
-        this.fields = this.gameHolder.querySelectorAll('.game__field');
-        this.colorToClass = {
+        this._nextMoveButton = document.querySelector(
+            this.getAttribute('next-move-selector')
+        );
+        this._followMovesButton = document.querySelector(
+            this.getAttribute('follow-moves-selector')
+        );
+        this._game = GameModel.fromObject(game);
+        this._numberOfCurrentMoveInView = this._game.numberOfMoves();
+        this._fields = this._gameHolder.querySelectorAll('.game__field');
+        this._colorToClass = {
             1: 'game__field--red',
             2: 'game__field--yellow'
         };
 
-        this.showMovesUpTo(this.numberOfCurrentMoveInView);
-        this.registerEventHandler();
+        this._showMovesUpTo(this._numberOfCurrentMoveInView);
+
+        this._registerEventHandler();
     }
 
     /**
      * @param {Number} index
      */
-    showMovesUpTo(index)
+    _showMovesUpTo(index)
     {
         // Clear fields
-        this.fields.forEach((field) => {
+        this._fields.forEach((field) => {
             field.classList.remove('game__field--flash');
             field.classList.remove('game__field--red');
             field.classList.remove('game__field--yellow');
         });
 
-        this.game.moves.slice(0, index).forEach(this.showMove.bind(this));
-        this.updateNavigationButtons();
+        this._game.moves.slice(0, index).forEach(this._showMove.bind(this));
+        this._updateNavigationButtons();
     }
 
     /**
@@ -60,14 +81,14 @@ window.Gaming.ConnectFour.Game = class
      *
      * @param {{x:Number, y:Number, color:Number}} move
      */
-    showMove(move)
+    _showMove(move)
     {
-        let field = this.gameHolder.querySelector('.game__field[data-point="' + move.x + ' ' + move.y + '"]');
+        let field = this._gameHolder.querySelector('.game__field[data-point="' + move.x + ' ' + move.y + '"]');
         field.classList.add(
-            this.colorToClass[move.color]
+            this._colorToClass[move.color]
         );
 
-        this.fields.forEach((field) => {
+        this._fields.forEach((field) => {
             field.classList.remove('game__field--flash');
         });
         field.classList.add('game__field--flash');
@@ -77,23 +98,23 @@ window.Gaming.ConnectFour.Game = class
      * Updates the previous move, next move und follow moves button according to the state of
      * the number of the current move in view.
      */
-    updateNavigationButtons()
+    _updateNavigationButtons()
     {
-        let isCurrentMoveTheLastMove = this.numberOfCurrentMoveInView === this.game.numberOfMoves();
-        let isCurrentMoveBeforeTheFirstMove = this.numberOfCurrentMoveInView === 0;
+        let isCurrentMoveTheLastMove = this._numberOfCurrentMoveInView === this._game.numberOfMoves();
+        let isCurrentMoveBeforeTheFirstMove = this._numberOfCurrentMoveInView === 0;
 
-        this.nextMoveButton.disabled = isCurrentMoveTheLastMove;
-        this.followMovesButton.disabled = isCurrentMoveTheLastMove;
-        this.previousMoveButton.disabled = isCurrentMoveBeforeTheFirstMove;
+        this._nextMoveButton.disabled = isCurrentMoveTheLastMove;
+        this._followMovesButton.disabled = isCurrentMoveTheLastMove;
+        this._previousMoveButton.disabled = isCurrentMoveBeforeTheFirstMove;
 
-        this.nextMoveButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
-        this.followMovesButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
-        this.previousMoveButton.classList.toggle('disabled', isCurrentMoveBeforeTheFirstMove);
+        this._nextMoveButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
+        this._followMovesButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
+        this._previousMoveButton.classList.toggle('disabled', isCurrentMoveBeforeTheFirstMove);
 
         // Remove flashing if the user follow the moves.
         if (isCurrentMoveTheLastMove) {
-            this.followMovesButton.classList.remove('button--yellow');
-            this.followMovesButton.classList.remove('button--flash');
+            this._followMovesButton.classList.remove('button--yellow');
+            this._followMovesButton.classList.remove('button--flash');
         }
     }
 
@@ -102,86 +123,86 @@ window.Gaming.ConnectFour.Game = class
      *
      * @param {{x:Number, y:Number, color:Number}} move
      */
-    onMoveAppendedToGame(move)
+    _onMoveAppendedToGame(move)
     {
         // Only show if the the user follow the moves. Otherwise notify user that a new move is available.
-        if (this.followMovesButton.disabled === true) {
-            this.showMove(move);
-            this.numberOfCurrentMoveInView++;
-            this.updateNavigationButtons();
+        if (this._followMovesButton.disabled === true) {
+            this._showMove(move);
+            this._numberOfCurrentMoveInView++;
+            this._updateNavigationButtons();
         } else {
-            this.followMovesButton.classList.add('button--yellow');
-            this.followMovesButton.classList.add('button--flash');
+            this._followMovesButton.classList.add('button--yellow');
+            this._followMovesButton.classList.add('button--flash');
         }
     }
 
-    onFieldClick(event)
+    _onFieldClick(event)
     {
         let cell = event.target;
 
-        this.gameHolder.classList.add('loading-indicator');
+        this._gameHolder.classList.add('loading-indicator');
 
         service.move(
-            this.game.gameId,
+            this._game.gameId,
             cell.dataset.column
         ).then(() => {
-            this.gameHolder.classList.remove('loading-indicator');
+            this._gameHolder.classList.remove('loading-indicator');
         }).catch(() => {
             // todo: Handle exception based on error
-            this.gameHolder.classList.remove('loading-indicator');
+            this._gameHolder.classList.remove('loading-indicator');
         });
     }
 
-    onPlayerMoved(event)
+    _onPlayerMoved(event)
     {
-        this.game.appendMove({
-            x: event.payload.x,
-            y: event.payload.y,
-            color: event.payload.color,
+        this._game.appendMove({
+            x: event.detail.x,
+            y: event.detail.y,
+            color: event.detail.color,
         });
     }
 
-    onPreviousMoveClick(event)
+    _onPreviousMoveClick(event)
     {
         event.preventDefault();
 
-        this.numberOfCurrentMoveInView--;
-        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+        this._numberOfCurrentMoveInView--;
+        this._showMovesUpTo(this._numberOfCurrentMoveInView);
     }
 
-    onNextMoveClick(event)
+    _onNextMoveClick(event)
     {
         event.preventDefault();
 
-        this.numberOfCurrentMoveInView++;
-        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+        this._numberOfCurrentMoveInView++;
+        this._showMovesUpTo(this._numberOfCurrentMoveInView);
     }
 
-    onFollowMovesClick(event)
+    _onFollowMovesClick(event)
     {
         event.preventDefault();
 
-        this.numberOfCurrentMoveInView = this.game.numberOfMoves();
-        this.showMovesUpTo(this.numberOfCurrentMoveInView);
+        this._numberOfCurrentMoveInView = this._game.numberOfMoves();
+        this._showMovesUpTo(this._numberOfCurrentMoveInView);
     }
 
-    registerEventHandler()
+    _registerEventHandler()
     {
-        this.game.onMoveAppended(this.onMoveAppendedToGame.bind(this));
+        this._game.onMoveAppended(this._onMoveAppendedToGame.bind(this));
 
-        this.eventPublisher.subscribe({
-            isSubscribedTo: (event) => {
-                return event.name === 'ConnectFour.PlayerMoved';
-            },
-            handle: this.onPlayerMoved.bind(this)
+        window.addEventListener(
+            'ConnectFour.PlayerMoved',
+            this._onPlayerMoved.bind(this)
+        );
+
+        this._fields.forEach((field) => {
+            field.addEventListener('click', this._onFieldClick.bind(this));
         });
 
-        this.fields.forEach((field) => {
-            field.addEventListener('click', this.onFieldClick.bind(this));
-        });
-
-        this.previousMoveButton.addEventListener('click', this.onPreviousMoveClick.bind(this));
-        this.nextMoveButton.addEventListener('click', this.onNextMoveClick.bind(this));
-        this.followMovesButton.addEventListener('click', this.onFollowMovesClick.bind(this));
+        this._previousMoveButton.addEventListener('click', this._onPreviousMoveClick.bind(this));
+        this._nextMoveButton.addEventListener('click', this._onNextMoveClick.bind(this));
+        this._followMovesButton.addEventListener('click', this._onFollowMovesClick.bind(this));
     }
-};
+}
+
+customElements.define('connect-four-game', GameElement);
