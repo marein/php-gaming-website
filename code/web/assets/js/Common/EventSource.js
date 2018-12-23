e8907aa9aaebd94ae7ef3c7530d30742f23781c9
Markus Reinhold <markusreinhold@icloud.com>
2018-12-23 13:37:05 +0100

    Remove unused old js #18
    * Remove old EventPublisher
    * Remove bootstrap.js

--- code/web/assets/js/Common/EventSource.js
+++ code/web/assets/js/Common/EventSource.js
@@ -34,21 +34,6 @@ class EventSourceElement extends HTMLElement
         );
 
         this._logOnVerbose(eventName, payload);
-
-        // Dispatch again to satisfy the old js design.
-        // todo: Remove this as soon as https://github.com/marein/php-gaming-website/issues/18 is done.
-        this.dispatchEvent(
-            new CustomEvent(
-                'event-for-deprecated-publisher',
-                {
-                    bubbles: true,
-                    detail: {
-                        name: eventName,
-                        payload: payload
-                    },
-                }
-            )
-        );
     }
 
     _logOnVerbose(eventName, payload)
