8738a103d25dabd73eb40801d9123c2630658e18
Markus Reinhold <markusreinhold@icloud.com>
2019-07-03 23:56:48 +0200

    Update dependencies
    Breaking changes in codeception:
    * You must use a trait for using friend sessions.
    
    Removing symfony/contracts (v1.0.2)
    Removing psr/simple-cache (1.0.1)
    Updating doctrine/collections (v1.6.1 => v1.6.2)
    Updating symfony/routing (v4.2.7 => v4.3.2)
    Installing symfony/polyfill-php73 (v1.11.0)
    Installing symfony/polyfill-intl-idn (v1.11.0)
    Installing symfony/mime (v4.3.2)
    Updating symfony/http-foundation (v4.2.7 => v4.3.2)
    Installing symfony/event-dispatcher-contracts (v1.1.5)
    Updating symfony/event-dispatcher (v4.2.7 => v4.3.2)
    Updating symfony/debug (v4.2.7 => v4.3.2)
    Updating symfony/http-kernel (v4.2.7 => v4.3.2)
    Updating symfony/finder (v4.2.7 => v4.3.2)
    Updating symfony/filesystem (v4.2.7 => v4.3.2)
    Installing symfony/service-contracts (v1.1.5)
    Updating symfony/dependency-injection (v4.2.7 => v4.3.2)
    Updating symfony/config (v4.2.7 => v4.3.2)
    Updating symfony/var-exporter (v4.2.7 => v4.3.2)
    Installing symfony/cache-contracts (v1.1.5)
    Updating symfony/cache (v4.2.7 => v4.3.2)
    Updating symfony/framework-bundle (v4.2.7 => v4.3.2)
    Updating doctrine/lexer (v1.0.1 => 1.0.2)
    Updating symfony/doctrine-bridge (v4.2.7 => v4.3.2)
    Updating symfony/console (v4.2.7 => v4.3.2)
    Updating doctrine/doctrine-bundle (1.10.2 => 1.11.2)
    Updating symfony/stopwatch (v4.2.7 => v4.3.2)
    Updating doctrine/migrations (v2.0.2 => v2.1.0)
    Updating symfony/security-core (v4.2.7 => v4.3.2)
    Updating symfony/security-csrf (v4.2.7 => v4.3.2)
    Updating dunglas/angular-csrf-bundle (v1.1.0 => v1.2.0)
    Updating enqueue/amqp-tools (0.9.8 => 0.9.12)
    Updating enqueue/amqp-lib (0.9.8 => 0.9.12)
    Updating marein/php-nchan-client (2.0.0 => 2.0.1)
    Updating symfony/asset (v4.2.7 => v4.3.2)
    Updating symfony/dotenv (v4.2.7 => v4.3.2)
    Updating symfony/monolog-bridge (v4.2.7 => v4.3.2)
    Updating symfony/monolog-bundle (v3.3.1 => v3.4.0)
    Updating symfony/templating (v4.2.7 => v4.3.2)
    Updating symfony/yaml (v4.2.7 => v4.3.2)
    Updating symfony/dom-crawler (v4.2.7 => v4.3.2)
    Updating symfony/css-selector (v4.2.7 => v4.3.2)
    Updating symfony/browser-kit (v4.2.7 => v4.3.2)
    Installing hoa/exception (1.17.01.16)
    Installing hoa/consistency (1.17.05.02)
    Installing hoa/event (1.17.01.13)
    Installing hoa/ustring (4.17.01.16)
    Installing hoa/protocol (1.17.01.14)
    Installing hoa/stream (1.17.02.21)
    Installing hoa/iterator (2.17.01.10)
    Installing hoa/file (1.17.07.11)
    Installing hoa/console (3.17.05.02)
    Updating ralouphie/getallheaders (2.0.5 => 3.0.3)
    Updating guzzlehttp/psr7 (1.5.2 => 1.6.1)
    Updating symfony/process (v4.2.7 => v4.3.2)
    Updating facebook/webdriver (1.6.0 => 1.7.1)
    Installing sebastian/type (1.1.3)
    Updating sebastian/global-state (2.0.0 => 3.0.0)
    Updating sebastian/environment (4.2.1 => 4.2.2)
    Updating phpunit/php-timer (2.1.1 => 2.1.2)
    Updating theseer/tokenizer (1.1.2 => 1.1.3)
    Updating phpunit/php-code-coverage (6.1.4 => 7.0.5)
    Updating phpdocumentor/reflection-docblock (4.3.0 => 4.3.1)
    Updating phpspec/prophecy (1.8.0 => 1.8.1)
    Updating phpunit/phpunit (7.5.9 => 8.2.4)
    Updating codeception/phpunit-wrapper (7.7.1 => 8.0.4)
    Updating codeception/codeception (2.5.6 => 3.0.2)
    Updating phpstan/phpdoc-parser (0.3.3 => 0.3.5)
    Updating nikic/php-parser (v4.2.1 => v4.2.2)
    Updating composer/xdebug-handler (1.3.2 => 1.3.3)
    Updating phpstan/phpstan (0.11.5 => 0.11.8)
    Updating symfony/var-dumper (v4.2.7 => v4.3.2)
    Updating twig/twig (v2.9.0 => v2.11.3)
    Installing symfony/translation-contracts (v1.1.5)
    Updating symfony/twig-bridge (v4.2.7 => v4.3.2)
    Updating symfony/debug-bundle (v4.2.7 => v4.3.2)
    Updating symfony/twig-bundle (v4.2.7 => v4.3.2)
    Updating symfony/web-profiler-bundle (v4.2.7 => v4.3.2)

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -13,13 +13,13 @@ declare(strict_types=1);
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
  *
  * @SuppressWarnings(PHPMD)
  */
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
+    use \Codeception\Lib\Actor\Shared\Friend;
 
     /**
      * Wait for the current url.
