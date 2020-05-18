c2a136c3650a29d22c959f2b98c0c9e8ec9de816
Markus Reinhold <markusreinhold@icloud.com>
2020-05-18 17:03:01 +0200

    Use opcache.preload (#44)
    * Use opcache.preload #43

---
+++ config/preload.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace {
+
+    use Symfony\Component\Finder\Finder;
+
+    $projectDirectory = dirname(__DIR__);
+
+    require_once $projectDirectory . '/vendor/autoload.php';
+
+    require_once $projectDirectory . '/var/cache/prod/srcGaming_AppKernelProdContainer.preload.php';
+
+    $finder = Finder::create()
+        ->files()
+        ->in($projectDirectory . '/src')
+        ->name('*.php');
+
+    foreach ($finder as $file) {
+        require_once $file->getRealPath();
+    }
+}
