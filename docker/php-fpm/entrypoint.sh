d2456af3dd38e132548db372f9d5125bdc5557f2
Markus Reinhold <markusreinhold@icloud.com>
2021-03-14 11:15:27 +0100

    Run migrations in a transaction #41

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -5,11 +5,35 @@ then
     wait-for-tcp-server "$WAIT_FOR" 120
 fi
 
-bin/console doctrine:database:create --connection=chat --if-not-exists >/dev/null 2>/dev/null
-bin/console doctrine:database:create --connection=connect_four --if-not-exists >/dev/null 2>/dev/null
-bin/console doctrine:database:create --connection=identity --if-not-exists >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/chat/migrations.yml --db=chat --no-interaction >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/connect-four/migrations.yml --db=connect_four --no-interaction >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/identity/migrations.yml --db=identity --no-interaction >/dev/null 2>/dev/null
+bin/console doctrine:database:create \
+    --connection=chat \
+    --if-not-exists \
+     >/dev/null 2>/dev/null
+bin/console doctrine:database:create \
+    --connection=connect_four \
+    --if-not-exists \
+     >/dev/null 2>/dev/null
+bin/console doctrine:database:create \
+    --connection=identity \
+    --if-not-exists \
+     >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate \
+    --configuration=config/chat/migrations.yml \
+    --db=chat \
+    --no-interaction \
+    --all-or-nothing \
+     >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate \
+    --configuration=config/connect-four/migrations.yml \
+    --db=connect_four \
+    --no-interaction \
+    --all-or-nothing \
+     >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate \
+    --configuration=config/identity/migrations.yml \
+    --db=identity \
+    --no-interaction \
+    --all-or-nothing \
+     >/dev/null 2>/dev/null
 
 exec "$@"
