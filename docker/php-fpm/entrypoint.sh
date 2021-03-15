aca4b684e28876077ed7f2c34fc5940669246d24
Markus Reinhold <markusreinhold@icloud.com>
2021-03-15 18:00:46 +0100

    Update dependencies #63

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -8,8 +8,8 @@ fi
 bin/console doctrine:database:create --connection=chat --if-not-exists >/dev/null 2>/dev/null
 bin/console doctrine:database:create --connection=connect_four --if-not-exists >/dev/null 2>/dev/null
 bin/console doctrine:database:create --connection=identity --if-not-exists >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/chat/migrations.yml --db=chat --no-interaction >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/connect-four/migrations.yml --db=connect_four --no-interaction >/dev/null 2>/dev/null
-bin/console doctrine:migrations:migrate --configuration=config/identity/migrations.yml --db=identity --no-interaction >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate --configuration=config/chat/migrations.yml --conn=chat --no-interaction >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate --configuration=config/connect-four/migrations.yml --conn=connect_four --no-interaction >/dev/null 2>/dev/null
+bin/console doctrine:migrations:migrate --configuration=config/identity/migrations.yml --conn=identity --no-interaction >/dev/null 2>/dev/null
 
 exec "$@"
