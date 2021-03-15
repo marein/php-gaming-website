237931b7a67ccf9ac52a631bbb4d8fa867e46f5b
Markus Reinhold <markusreinhold@icloud.com>
2021-03-15 20:42:01 +0100

    Move cli args to config file #41

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -18,20 +18,14 @@ bin/console doctrine:database:create \
     --if-not-exists
 bin/console doctrine:migrations:migrate \
     --configuration=config/chat/migrations.yml \
-    --conn=chat \
-    --all-or-nothing \
     --allow-no-migration \
     --no-interaction
 bin/console doctrine:migrations:migrate \
     --configuration=config/connect-four/migrations.yml \
-    --conn=connect_four \
-    --all-or-nothing \
     --allow-no-migration \
     --no-interaction
 bin/console doctrine:migrations:migrate \
     --configuration=config/identity/migrations.yml \
-    --conn=identity \
-    --all-or-nothing \
     --allow-no-migration \
     --no-interaction
 
