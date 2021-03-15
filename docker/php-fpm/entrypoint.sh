0298528bde00be55ea7fd12a6521d89228b53b14
Markus Reinhold <markusreinhold@icloud.com>
2021-03-15 22:44:41 +0100

    Run migrations with environment variable #41
    RUN_MIGRATIONS must be "1".

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -7,26 +7,29 @@ then
     wait-for-tcp-server "$WAIT_FOR" 120
 fi
 
-bin/console doctrine:database:create \
-    --connection=chat \
-    --if-not-exists
-bin/console doctrine:database:create \
-    --connection=connect_four \
-    --if-not-exists
-bin/console doctrine:database:create \
-    --connection=identity \
-    --if-not-exists
-bin/console doctrine:migrations:migrate \
-    --configuration=config/chat/migrations.yml \
-    --allow-no-migration \
-    --no-interaction
-bin/console doctrine:migrations:migrate \
-    --configuration=config/connect-four/migrations.yml \
-    --allow-no-migration \
-    --no-interaction
-bin/console doctrine:migrations:migrate \
-    --configuration=config/identity/migrations.yml \
-    --allow-no-migration \
-    --no-interaction
+if [ "$RUN_MIGRATIONS" = "1" ]
+then
+    bin/console doctrine:database:create \
+        --connection=chat \
+        --if-not-exists
+    bin/console doctrine:database:create \
+        --connection=connect_four \
+        --if-not-exists
+    bin/console doctrine:database:create \
+        --connection=identity \
+        --if-not-exists
+    bin/console doctrine:migrations:migrate \
+        --configuration=config/chat/migrations.yml \
+        --allow-no-migration \
+        --no-interaction
+    bin/console doctrine:migrations:migrate \
+        --configuration=config/connect-four/migrations.yml \
+        --allow-no-migration \
+        --no-interaction
+    bin/console doctrine:migrations:migrate \
+        --configuration=config/identity/migrations.yml \
+        --allow-no-migration \
+        --no-interaction
+fi
 
 exec "$@"
