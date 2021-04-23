7c396f4d5e3a26238e9e767001bfe0119297690e
Markus Reinhold <markusreinhold@icloud.com>
2021-04-23 16:09:49 +0200

    Use array_map instead of foreach #66

--- src/Common/Port/Adapter/Bus/SymfonyValidatorBus.php
+++ src/Common/Port/Adapter/Bus/SymfonyValidatorBus.php
@@ -7,6 +7,7 @@ use Gaming\Common\Bus\Bus;
 use Gaming\Common\Bus\Exception\ApplicationException;
 use Gaming\Common\Bus\Violation;
 use Gaming\Common\Bus\ViolationParameter;
+use Symfony\Component\Validator\ConstraintViolationInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -50,52 +51,41 @@ final class SymfonyValidatorBus implements Bus
         return $this->bus->handle($message);
     }
 
-    /**+
+    /**
      * Maps from symfony validation objects.
      *
-     * @param ConstraintViolationListInterface $symfonyViolations
+     * @param ConstraintViolationListInterface<ConstraintViolationInterface> $symfonyViolations
      *
      * @return Violation[]
      */
     private function mapFromSymfonyViolations(ConstraintViolationListInterface $symfonyViolations): array
     {
-        $violations = [];
-
-        foreach ($symfonyViolations as $symfonyViolation) {
-            $violations[] = new Violation(
-                $symfonyViolation->getPropertyPath(),
-                $symfonyViolation->getMessageTemplate(),
-                $this->mapFromSymfonyParameters($symfonyViolation->getParameters())
-            );
-        }
-
-        return $violations;
+        return array_map(
+            fn(ConstraintViolationInterface $constraintViolation): Violation => new Violation(
+                $constraintViolation->getPropertyPath(),
+                $constraintViolation->getMessageTemplate(),
+                $this->mapFromSymfonyParameters($constraintViolation->getParameters())
+            ),
+            iterator_to_array($symfonyViolations)
+        );
     }
 
     /**
      * Remove template characters from parameter names.
      *
-     * @param mixed[] $symfonyParameters
+     * @param array<string, bool|int|float|string> $symfonyParameters
      *
-     * @return mixed[]
+     * @return ViolationParameter[]
      */
     private function mapFromSymfonyParameters(array $symfonyParameters): array
     {
-        $parameters = [];
-
-        foreach ($symfonyParameters as $name => $value) {
-            $nameWithoutTemplateCode = str_replace(
-                ['{{ ', ' }}'],
-                '',
-                $name
-            );
-
-            $parameters[] = new ViolationParameter(
-                $nameWithoutTemplateCode,
+        return array_map(
+            fn(string $name, bool|int|float|string $value): ViolationParameter => new ViolationParameter(
+                str_replace(['{{ ', ' }}'], '', $name),
                 $value
-            );
-        }
-
-        return $parameters;
+            ),
+            array_keys($symfonyParameters),
+            $symfonyParameters
+        );
     }
 }
