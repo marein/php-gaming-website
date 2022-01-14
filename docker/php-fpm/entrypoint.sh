f4ba44a4d0812f96ca7e0599ae20991abc552c8e
Markus Reinhold <markusreinhold@icloud.com>
2022-01-15 00:07:14 +0100

    Split migrations per domain #41
    This is because each domain can have its own
    physical database, ignoring the others.

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -7,27 +7,35 @@ then
     wait-for-tcp-server "$WAIT_FOR" 120
 fi
 
-if [ "$RUN_MIGRATIONS" = "1" ]
+if [ "$RUN_CHAT_MIGRATIONS" = "1" ]
 then
     bin/console doctrine:database:create \
         --connection=chat \
-        --if-not-exists
-    bin/console doctrine:database:create \
-        --connection=connect_four \
-        --if-not-exists
-    bin/console doctrine:database:create \
-        --connection=identity \
         --if-not-exists
     bin/console doctrine:migrations:migrate \
         --configuration=config/chat/migrations.yml \
         --conn=chat \
         --allow-no-migration \
         --no-interaction
+fi
+
+if [ "$RUN_CONNECT_FOUR_MIGRATIONS" = "1" ]
+then
+    bin/console doctrine:database:create \
+        --connection=connect_four \
+        --if-not-exists
     bin/console doctrine:migrations:migrate \
         --configuration=config/connect-four/migrations.yml \
         --conn=connect_four \
         --allow-no-migration \
         --no-interaction
+fi
+
+if [ "$RUN_IDENTITY_MIGRATIONS" = "1" ]
+then
+    bin/console doctrine:database:create \
+        --connection=identity \
+        --if-not-exists
     bin/console doctrine:migrations:migrate \
         --configuration=config/identity/migrations.yml \
         --conn=identity \
