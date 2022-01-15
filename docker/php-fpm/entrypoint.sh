39826a1623b9110715b07593dfab4be2255d5a65
Markus Reinhold <markusreinhold@icloud.com>
2022-01-16 00:47:40 +0100

    Split migration statements in separate files #41

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -7,40 +7,6 @@ then
     wait-for-tcp-server "$WAIT_FOR" 120
 fi
 
-if [ "$RUN_CHAT_MIGRATIONS" = "1" ]
-then
-    bin/console doctrine:database:create \
-        --connection=chat \
-        --if-not-exists
-    bin/console doctrine:migrations:migrate \
-        --configuration=config/chat/migrations.yml \
-        --conn=chat \
-        --allow-no-migration \
-        --no-interaction
-fi
-
-if [ "$RUN_CONNECT_FOUR_MIGRATIONS" = "1" ]
-then
-    bin/console doctrine:database:create \
-        --connection=connect_four \
-        --if-not-exists
-    bin/console doctrine:migrations:migrate \
-        --configuration=config/connect-four/migrations.yml \
-        --conn=connect_four \
-        --allow-no-migration \
-        --no-interaction
-fi
-
-if [ "$RUN_IDENTITY_MIGRATIONS" = "1" ]
-then
-    bin/console doctrine:database:create \
-        --connection=identity \
-        --if-not-exists
-    bin/console doctrine:migrations:migrate \
-        --configuration=config/identity/migrations.yml \
-        --conn=identity \
-        --allow-no-migration \
-        --no-interaction
-fi
+find bin/*/onEntrypoint -print0 | xargs -0 -n 1 bash
 
 exec "$@"
