36340025670d099c39fe6238b784fde1b99281e7
Markus Reinhold <markusreinhold@icloud.com>
2022-05-17 23:53:33 +0200

    Setup grafana and prometheus #130
    Currently the scrape path is still hard coded.
    This commit just flushes the local repository copy.
    
    * Dashboards and datasources are provisioned via files.
    * Example metric for snapshot of running games is given.
    * User arrival doesn't happen on path '^/metrics/'.

--- src/WebInterface/Infrastructure/EventListener/AssignUserIdOnKernelRequest.php
+++ src/WebInterface/Infrastructure/EventListener/AssignUserIdOnKernelRequest.php
@@ -9,21 +9,19 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 final class AssignUserIdOnKernelRequest
 {
-    private IdentityService $identityService;
-
-    public function __construct(IdentityService $identityService)
-    {
-        $this->identityService = $identityService;
+    public function __construct(
+        private readonly IdentityService $identityService,
+        private readonly string $ignoredPathExpression
+    ) {
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
-        if (!$event->isMasterRequest()) {
+        if ($this->shouldIgnoreRequest($event)) {
             return;
         }
 
-        $request = $event->getRequest();
-        $session = $request->getSession();
+        $session = $event->getRequest()->getSession();
 
         if (!$session->has('user')) {
             // todo Create user only when it's really needed.
@@ -32,5 +30,11 @@ final class AssignUserIdOnKernelRequest
             // For example: open a game, sign up etc.
             $session->set('user', $this->identityService->arrive()['userId']);
         }
+    }
+
+    private function shouldIgnoreRequest(RequestEvent $event): bool
+    {
+        return !$event->isMainRequest()
+            || preg_match($this->ignoredPathExpression, $event->getRequest()->getRequestUri()) === 1;
     }
 }
