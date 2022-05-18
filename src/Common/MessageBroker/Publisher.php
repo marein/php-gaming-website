34d6bf04f4c3431ce37dca03cda966a5bf81a478
Markus Reinhold <markusreinhold@icloud.com>
2022-05-18 22:44:11 +0200

    Describe the use case of flush

--- src/Common/MessageBroker/Publisher.php
+++ src/Common/MessageBroker/Publisher.php
@@ -15,6 +15,14 @@ interface Publisher
     public function publish(Message $message): void;
 
     /**
+     * Flush can have different semantics depending on the technology used
+     * and the implementation of this interface. As a caller of publish,
+     * you must call flush after you are done publishing. The implementation
+     * chosen should determine whether any of the following use cases apply:
+     * * Make sure that all buffered messages are transferred to the server.
+     * * Make sure that all received messages are saved on the disk.
+     * * If the loss of messages is acceptable, the implementation can be empty.
+     *
      * @throws MessageBrokerException
      */
     public function flush(): void;
