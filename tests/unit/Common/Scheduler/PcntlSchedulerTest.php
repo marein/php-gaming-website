c8d2c9afca75843a449ff07275795285aded79ef
Markus Reinhold <markusreinhold@icloud.com>
2022-07-28 20:32:35 +0200

    Provide scheduler library (#132)
    The scheduler can be used to interrupt the application
    at a certain time to trigger certain handlers. It's an abstraction
    around pcntl_alarm with the advantage that multiple handlers
    can be registered. For example, heartbeats can be sent to
    applications such as RabbitMQ or MySQL to maintain
    connections in a long-running process without having to
    switch the execution model (e.g. fibers) of those processes.

---
+++ tests/unit/Common/Scheduler/PcntlSchedulerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gaming\Tests\Unit\Common\Scheduler;
+
+use Gaming\Common\Scheduler\Handler;
+use Gaming\Common\Scheduler\NullHandler;
+use Gaming\Common\Scheduler\PcntlScheduler;
+use Gaming\Common\Scheduler\Scheduler;
+use PHPUnit\Framework\TestCase;
+
+final class PcntlSchedulerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldSchedule(): void
+    {
+        $expectedCount = 2;
+
+        $testHandler = new class ($expectedCount) implements Handler {
+            public function __construct(
+                public readonly int $expectedCount,
+                public int $count = 0,
+            ) {
+            }
+
+            public function handle(Scheduler $scheduler): void
+            {
+                if (++$this->count === $this->expectedCount) {
+                    return;
+                }
+
+                $scheduler->schedule(time(), $this);
+            }
+        };
+
+        $scheduler = new PcntlScheduler();
+
+        $scheduler->schedule(
+            time(),
+            $testHandler
+        );
+
+        $scheduler->schedule(
+            time(),
+            new NullHandler()
+        );
+
+        // This test needs to sleep, otherwise the SIGALRM implementation cannot be tested.
+        $seconds = 3;
+        while ($seconds = sleep($seconds));
+
+        self::assertSame($expectedCount, $testHandler->count);
+    }
+}
