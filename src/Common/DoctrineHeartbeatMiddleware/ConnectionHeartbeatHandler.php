6dad8e7339655369871a5b3c4aa20f44ade85288
Markus Reinhold <markusreinhold@icloud.com>
2022-08-01 19:44:34 +0200

    Use WeakReference to detect unused connections

--- src/Common/DoctrineHeartbeatMiddleware/ConnectionHeartbeatHandler.php
+++ src/Common/DoctrineHeartbeatMiddleware/ConnectionHeartbeatHandler.php
@@ -6,23 +6,41 @@ namespace Gaming\Common\DoctrineHeartbeatMiddleware;
 
 use Gaming\Common\Scheduler\Handler;
 use Gaming\Common\Scheduler\Scheduler;
+use WeakReference;
 
 final class ConnectionHeartbeatHandler implements Handler
 {
+    /**
+     * Without a method like Doctrine\DBAL\Driver\Connection::isConnected(),
+     * this handler cannot know if the connection is still open, so the WeakReference is used.
+     * If no one has a reference to the connection anymore, the handler can safely terminate.
+     * This will not keep the connection open forever.
+     *
+     * @var WeakReference<TrackActivityConnection>
+     */
+    private readonly WeakReference $connectionWeakReference;
+
     public function __construct(
-        private readonly TrackActivityConnection $connection,
+        TrackActivityConnection $connection,
         private readonly string $dummySql,
         private readonly int $interval,
     ) {
+        $this->connectionWeakReference = WeakReference::create($connection);
     }
 
     public function handle(Scheduler $scheduler): void
     {
+        $connection = $this->connectionWeakReference->get();
+        if ($connection === null) {
+            return;
+        }
+        assert($connection instanceof TrackActivityConnection);
+
         if (
-            !$this->connection->isWriting()
-            && ($this->connection->lastActivity() + $this->interval) < time()
+            !$connection->isWriting()
+            && ($connection->lastActivity() + $this->interval) < time()
         ) {
-            $this->connection->query($this->dummySql);
+            $connection->query($this->dummySql);
         }
 
         $scheduler->schedule(time() + $this->interval, $this);
