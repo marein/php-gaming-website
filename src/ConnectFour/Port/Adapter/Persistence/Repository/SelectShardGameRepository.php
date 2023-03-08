68d41c52d0ab69553fe4dffae63de42387ec9d5e
Markus Reinhold <markusreinhold@icloud.com>
2023-03-08 22:05:29 +0100

    Shard MySQL for connect four context
    * Apply schema based sharding
    The shard is determined by a modulo operation.
    * Make database name configurable via env variable
    Doctrine commands aren't accepting a database name as a cli
    argument. By overwriting the env variable, see entrypoint,
    we can change the database without changing the whole connection
    string.
    * Acceptance tests are disabled for now
    This reason is that the number of shards isn't yet configurable.

---
+++ src/ConnectFour/Port/Adapter/Persistence/Repository/SelectShardGameRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gaming\ConnectFour\Port\Adapter\Persistence\Repository;
+
+use Doctrine\DBAL\Connection;
+use Gaming\ConnectFour\Domain\Game\Game;
+use Gaming\ConnectFour\Domain\Game\GameId;
+use Gaming\ConnectFour\Domain\Game\Games;
+use mysqli;
+
+final class SelectShardGameRepository implements Games
+{
+    public function __construct(
+        private readonly Games $games,
+        private readonly Connection $connection,
+        private readonly string $databaseNamePrefix,
+        private readonly int $numberOfShards
+    ) {
+    }
+
+    public function nextIdentity(): GameId
+    {
+        return $this->games->nextIdentity();
+    }
+
+    /**
+     * @throw ConcurrencyException
+     */
+    public function save(Game $game): void
+    {
+        $this->selectDatabase($game->id());
+
+        $this->games->save($game);
+    }
+
+    public function get(GameId $id): Game
+    {
+        $this->selectDatabase($id);
+
+        return $this->games->get($id);
+    }
+
+    private function selectDatabase(GameId $gameId): void
+    {
+        $mysqli = $this->connection->getNativeConnection();
+        assert($mysqli instanceof mysqli);
+
+        $mysqli->select_db(
+            $this->databaseNamePrefix . ((crc32($gameId->toString()) % $this->numberOfShards) + 1)
+        );
+    }
+}
