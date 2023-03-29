bb8b3eddcf86c1e3fc17d9f0b484b01178447a05
Markus Reinhold <markusreinhold@icloud.com>
2023-03-29 22:59:30 +0200

    Add comment for the repository

--- src/ConnectFour/Domain/Game/Games.php
+++ src/ConnectFour/Domain/Game/Games.php
@@ -8,6 +8,11 @@ use Closure;
 use Gaming\Common\Domain\Exception\ConcurrencyException;
 use Gaming\ConnectFour\Domain\Game\Exception\GameNotFoundException;
 
+/**
+ * This repository is very persistence oriented to highlight the transactional scope (technical-wise).
+ * Games can be sharded across multiple physical databases, hence the transactional scope is applied
+ * in the repository and not in the application layer.
+ */
 interface Games
 {
     public function nextIdentity(): GameId;
