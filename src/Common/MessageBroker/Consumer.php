729bb790e06d5420f508b2249abfb1fdafedb198
Markus Reinhold <markusreinhold@icloud.com>
2023-06-22 01:46:27 +0200

    Add @throws docblock

--- src/Common/MessageBroker/Consumer.php
+++ src/Common/MessageBroker/Consumer.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Gaming\Common\MessageBroker;
 
+use Gaming\Common\MessageBroker\Exception\MessageBrokerException;
+
 interface Consumer
 {
     /**
@@ -13,8 +15,13 @@ interface Consumer
      * ReplySent and RequestSent through a PSR-14 compliant event dispatcher.
      *
      * @param positive-int $parallelism
+     *
+     * @throws MessageBrokerException
      */
     public function start(int $parallelism): void;
 
+    /**
+     * @throws MessageBrokerException
+     */
     public function stop(): void;
 }
