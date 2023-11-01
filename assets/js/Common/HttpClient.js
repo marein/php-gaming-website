074a5978aa4b934a5aa424321115a026af40870e
Markus Reinhold <markusreinhold@icloud.com>
2023-11-01 11:44:16 +0100

    Use fetch everywhere

--- assets/js/Common/HttpClient.js
+++ assets/js/Common/HttpClient.js
@@ -1,7 +1,6 @@
 class HttpClient {
     constructor() {
         this.onError = (response) => {
-
         };
     }
 
@@ -9,52 +8,35 @@ class HttpClient {
      * @param {String} url
      * @returns {Promise}
      */
-    get(url) {
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-            request.addEventListener('load', () => {
-                let response = JSON.parse(request.responseText);
-
-                if (request.status >= 200 && request.status < 300) {
-                    resolve(response);
-                } else {
-                    this.onError(response);
-                    reject(response);
-                }
-            });
-            request.send();
+    async get(url) {
+        const response = await fetch(url, {
+            headers: {'X-Requested-With': 'XMLHttpRequest'}
         });
+        const json = await response.json();
+
+        if (response.status >= 200 && response.status < 300) return json;
+
+        this.onError(json);
+        throw json;
     }
 
     /**
      * @param {String} url
-     * @param {Object} [postParameters]
+     * @param {Object} data
      * @returns {Promise}
      */
-    post(url, postParameters) {
-        postParameters = postParameters || {};
-
-        return new Promise((resolve, reject) => {
-            let request = new XMLHttpRequest();
-            request.open('POST', url);
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-            request.addEventListener('load', () => {
-                let response = JSON.parse(request.responseText);
-
-                if (request.status >= 200 && request.status < 300) {
-                    resolve(response);
-                } else {
-                    this.onError(response);
-                    reject(response);
-                }
-            });
-            request.send(
-                this._preparePostParameters(postParameters)
-            );
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            body: new URLSearchParams(data)
         });
+        const json = await response.json();
+
+        if (response.status >= 200 && response.status < 300) return json;
+
+        this.onError(json);
+        throw json;
     }
 
     /**
@@ -62,21 +44,6 @@ class HttpClient {
      */
     redirectTo(url) {
         window.app.navigate(url);
-    }
-
-    /**
-     * @param {Object} postParameters
-     * @returns {String}
-     */
-    _preparePostParameters(postParameters) {
-        let preparedPostParameters = [];
-
-        for (let name in postParameters) {
-            let value = postParameters[name];
-            preparedPostParameters.push(name + '=' + encodeURIComponent(value));
-        }
-
-        return preparedPostParameters.join('&');
     }
 }
 
