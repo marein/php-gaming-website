79e8937e29722d36e48b1d789c92a334c21440eb
Markus Reinhold <markusreinhold@icloud.com>
2023-12-27 18:45:33 +0100

    EventStore streaming improvements
    * Dispatch events via PSR-14 event dispatcher so that
    clients can hook into the lifecycle.
    * Move debug logs to an event listener instead of using
    a decorator.
    * Add a listener that resets services to avoid possible
    memory leaks.
    * Encapsulate EventStore dispatcher code in a single class
    to improve readability.
    * Improve UI by adding boxes and colors.
    * Use a comma-separated list to specify which subscribers
    to use.
    * Rename argument -w to -p to specify how many events are
    processed in parallel. Default is now set to 1.
    
    Related adjustments for ForkPool:
    * Use stream_select before reading from a channel to get
    immediate signal handling.
    * Make each fork a session leader so that only the main process
    receives SIGINT when the command is interrupted in the terminal.

--- src/Common/EventStore/FollowEventStoreDispatcher.php
+++ src/Common/EventStore/FollowEventStoreDispatcher.php
@@ -4,40 +4,57 @@ declare(strict_types=1);
 
 namespace Gaming\Common\EventStore;
 
+use Gaming\Common\EventStore\Event\EventsCommitted;
+use Gaming\Common\EventStore\Event\EventsFetched;
 use Gaming\Common\EventStore\Exception\EventStoreException;
-use Gaming\Common\EventStore\Exception\FailedRetrieveMostRecentPublishedStoredEventIdException;
-use Gaming\Common\EventStore\Exception\FailedTrackMostRecentPublishedStoredEventIdException;
-use InvalidArgumentException;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 final class FollowEventStoreDispatcher
 {
+    private bool $shouldStop;
+
     public function __construct(
+        private readonly PollableEventStore $pollableEventStore,
+        private EventStorePointer $eventStorePointer,
         private readonly StoredEventSubscriber $storedEventSubscriber,
-        private readonly EventStorePointer $eventStorePointer,
-        private readonly PollableEventStore $pollableEventStore
+        private readonly int $batchSize,
+        private readonly int $throttleTimeInMilliseconds,
+        private readonly EventDispatcherInterface $eventDispatcher
     ) {
+        $this->eventStorePointer = new InMemoryCacheEventStorePointer($eventStorePointer);
+        $this->shouldStop = false;
     }
 
     /**
      * @throws EventStoreException
-     * @throws FailedRetrieveMostRecentPublishedStoredEventIdException
-     * @throws FailedTrackMostRecentPublishedStoredEventIdException
      */
-    public function dispatch(int $batchSize): int
+    public function start(): void
     {
-        if ($batchSize < 1) {
-            throw new InvalidArgumentException('batchSize must be greater than 0');
+        while (!$this->shouldStop) {
+            $this->handleNextBatch();
         }
+    }
 
-        $lastStoredEventId = $this->eventStorePointer->retrieveMostRecentPublishedStoredEventId();
+    public function stop(): void
+    {
+        $this->shouldStop = true;
+    }
 
+    /**
+     * @throws EventStoreException
+     */
+    private function handleNextBatch(): void
+    {
         $storedEvents = $this->pollableEventStore->since(
-            $lastStoredEventId,
-            $batchSize
+            $this->eventStorePointer->retrieveMostRecentPublishedStoredEventId(),
+            $this->batchSize
         );
+        $this->eventDispatcher->dispatch(new EventsFetched($storedEvents));
 
         if (count($storedEvents) === 0) {
-            return 0;
+            $this->eventDispatcher->dispatch(new EventsCommitted([]));
+            usleep($this->throttleTimeInMilliseconds * 1000);
+            return;
         }
 
         foreach ($storedEvents as $storedEvent) {
@@ -45,10 +62,7 @@ final class FollowEventStoreDispatcher
         }
 
         $this->storedEventSubscriber->commit();
-
-        $lastStoredEventId = end($storedEvents)->id();
-        $this->eventStorePointer->trackMostRecentPublishedStoredEventId($lastStoredEventId);
-
-        return count($storedEvents);
+        $this->eventStorePointer->trackMostRecentPublishedStoredEventId(end($storedEvents)->id());
+        $this->eventDispatcher->dispatch(new EventsCommitted($storedEvents));
     }
 }
