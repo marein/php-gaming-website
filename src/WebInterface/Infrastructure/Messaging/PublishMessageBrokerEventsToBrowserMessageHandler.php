bbac68a7ebd865e5125b0506a602eb9f77944dd1
Markus Reinhold <markusreinhold@icloud.com>
2024-01-08 12:49:04 +0100

    Route messages to browser based on streamId

--- src/WebInterface/Infrastructure/Messaging/PublishMessageBrokerEventsToBrowserMessageHandler.php
+++ src/WebInterface/Infrastructure/Messaging/PublishMessageBrokerEventsToBrowserMessageHandler.php
@@ -20,101 +20,29 @@ final class PublishMessageBrokerEventsToBrowserMessageHandler implements Message
     public function handle(Message $message, Context $context): void
     {
         match ($message->name()) {
-            'ConnectFour.GameOpened' => $this->handleGameOpened($message),
-            'ConnectFour.GameAborted' => $this->handleGameAborted($message),
-            'ConnectFour.GameResigned' => $this->handleGameResigned($message),
-            'ConnectFour.GameWon' => $this->handleGameWon($message),
-            'ConnectFour.GameDrawn' => $this->handleGameDrawn($message),
-            'ConnectFour.PlayerMoved' => $this->handlePlayerMoved($message),
-            'ConnectFour.ChatAssigned' => $this->handleChatAssigned($message),
-            'ConnectFour.PlayerJoined' => $this->handlePlayerJoined($message),
+            'ConnectFour.GameOpened',
+            'ConnectFour.GameAborted' => $this->browserNotifier->publish(
+                ['lobby'],
+                $message->name(),
+                $message->body()
+            ),
+            'ConnectFour.GameResigned',
+            'ConnectFour.GameWon',
+            'ConnectFour.GameDrawn',
+            'ConnectFour.PlayerMoved',
+            'ConnectFour.ChatAssigned' => $this->browserNotifier->publish(
+                ['connect-four-' . $message->streamId()],
+                $message->name(),
+                $message->body()
+            ),
+            'ConnectFour.PlayerJoined' => $this->browserNotifier->publish(
+                ['lobby', 'connect-four-' . $message->streamId()],
+                $message->name(),
+                $message->body()
+            ),
             'Chat.MessageWritten' => $this->handleMessageWritten($message),
             default => true
         };
-    }
-
-    private function handleGameOpened(Message $message): void
-    {
-        $this->browserNotifier->publish(
-            ['lobby'],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handleGameAborted(Message $message): void
-    {
-        $this->browserNotifier->publish(
-            ['lobby'],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handlePlayerJoined(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['lobby', 'connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handleGameResigned(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handleGameWon(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handleGameDrawn(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handlePlayerMoved(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
-    }
-
-    private function handleChatAssigned(Message $message): void
-    {
-        $event = json_decode($message->body(), true, 512, JSON_THROW_ON_ERROR);
-
-        $this->browserNotifier->publish(
-            ['connect-four-' . $event['gameId']],
-            $message->name(),
-            $message->body()
-        );
     }
 
     private function handleMessageWritten(Message $message): void
@@ -123,7 +51,7 @@ final class PublishMessageBrokerEventsToBrowserMessageHandler implements Message
         $event->mergeFromString($message->body());
 
         $this->browserNotifier->publish(
-            ['chat-' . $event->getChatId()],
+            ['chat-' . $message->streamId()],
             $message->name(),
             $event->serializeToJsonString()
         );
