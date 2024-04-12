622435157fa8cecb89f94b5ba3352505f9957717
Markus Reinhold <markusreinhold@icloud.com>
2024-04-12 22:00:14 +0200

    Resolve doctrine deprecations (#221)
    Embed dbname in url instead of configuring it separately.

--- src/Common/Bus/Integration/DoctrineTransactionalBus.php
+++ src/Common/Bus/Integration/DoctrineTransactionalBus.php
@@ -5,35 +5,20 @@ declare(strict_types=1);
 namespace Gaming\Common\Bus\Integration;
 
 use Doctrine\DBAL\Connection;
-use Exception;
 use Gaming\Common\Bus\Bus;
 
 final class DoctrineTransactionalBus implements Bus
 {
-    private Bus $bus;
-
-    private Connection $connection;
-
-    public function __construct(Bus $bus, Connection $connection)
-    {
-        $this->bus = $bus;
-        $this->connection = $connection;
+    public function __construct(
+        private readonly Bus $bus,
+        private readonly Connection $connection
+    ) {
     }
 
     public function handle(object $message): mixed
     {
-        $this->connection->beginTransaction();
-
-        try {
-            $return = $this->bus->handle($message);
-
-            $this->connection->commit();
-
-            return $return;
-        } catch (Exception $exception) {
-            $this->connection->rollBack();
-
-            throw $exception;
-        }
+        return $this->connection->transactional(
+            fn() => $this->bus->handle($message)
+        );
     }
 }
