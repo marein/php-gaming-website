a18b79049e3ab07af1e70136cca29aedb1b3a6c2
Markus Reinhold <markusreinhold@icloud.com>
2024-07-24 17:32:55 +0200

    Add logout functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import {client} from './Common/HttpClient.js'
 import './Common/NotificationList.js'
 import '@tabler/core/dist/css/tabler.min.css'
+import '@tabler/core/dist/js/tabler.min.js'
 import '../css/app.css'
 
 window.app = {
@@ -24,7 +25,12 @@ window.app = {
 client.onError = response => document.querySelector('notification-list').appendMessage(response.message);
 
 window.app.peInit();
-window.addEventListener('pe:click', e => e.detail.a.closest('[data-no-turbolink]') && e.preventDefault());
+window.addEventListener('pe:click', e => {
+    (
+        e.detail.a.hasAttribute('data-bs-toggle') ||
+        e.detail.a.closest('[data-no-turbolink]')
+    ) && e.preventDefault()
+});
 window.addEventListener('pe:navigate', e => {
     e.detail.parsed.push(dom => window.app.loadElements(dom.body));
     e.detail.succeed.push(() => window.dispatchEvent(new CustomEvent('app:load')));
--- config/importmap.php
+++ config/importmap.php
@@ -20,7 +20,8 @@ namespace {
         'event-source' => ['path' => 'js/Common/EventSource.js'],
         'notification-list' => ['path' => 'js/Common/NotificationList.js'],
         'uhtml/node.js' => ['version' => '4.5.8'],
-        '@tabler/core/dist/css/tabler.min.css' => ['version' => '1.0.0-beta20', 'type' => 'css']
+        '@tabler/core/dist/css/tabler.min.css' => ['version' => '1.0.0-beta20', 'type' => 'css'],
+        '@tabler/core/dist/js/tabler.min.js' => ['version' => '1.0.0-beta20']
     ];
 
     foreach ($finder as $file) {
