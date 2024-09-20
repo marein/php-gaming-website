eadc041e9a8cd73501a73f767360116a0f6d3373
Markus Reinhold <markusreinhold@icloud.com>
2024-09-20 18:04:21 +0200

    Add stack files and load test scenario
    * The stack is deployed with docker swarm.
    * The test scenarios are running with k6.
    * Remove old load script bin/createLoad.
    * Remove endpoints used exclusively by the old script.

---
+++ deploy/load-test/scenario/go-to-page.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+
+const baseUrl = __ENV.BASE_URL;
+const pageUrl = __ENV.PAGE_URL;
+const headers = {'Origin': baseUrl};
+const cookieJar = new http.CookieJar();
+
+export default function () {
+    for (let i = 0; i < 100; i++) {
+        http.get(baseUrl + (pageUrl || '/'), {}, {cookieJar, headers});
+    }
+}
---
+++ deploy/load-test/scenario/play-connect-four.js
@@ -0,0 +1,48 @@
+import http from 'k6/http';
+
+const baseUrl = __ENV.BASE_URL;
+const headers = {'Origin': baseUrl};
+const jarPlayerOne = new http.CookieJar();
+const jarPlayerTwo = new http.CookieJar();
+
+export default function () {
+    const gameId = open(jarPlayerOne);
+    join(jarPlayerTwo, gameId);
+    move(jarPlayerOne, gameId, 1);
+    move(jarPlayerTwo, gameId, 2);
+    move(jarPlayerOne, gameId, 3);
+    move(jarPlayerTwo, gameId, 4);
+    move(jarPlayerOne, gameId, 5);
+    move(jarPlayerTwo, gameId, 6);
+    move(jarPlayerOne, gameId, 7);
+    move(jarPlayerTwo, gameId, 1);
+    move(jarPlayerOne, gameId, 2);
+    move(jarPlayerTwo, gameId, 3);
+    move(jarPlayerOne, gameId, 4);
+    move(jarPlayerTwo, gameId, 5);
+    move(jarPlayerOne, gameId, 6);
+    move(jarPlayerTwo, gameId, 7);
+    move(jarPlayerOne, gameId, 1);
+    move(jarPlayerTwo, gameId, 2);
+    move(jarPlayerOne, gameId, 3);
+    move(jarPlayerTwo, gameId, 4);
+    move(jarPlayerOne, gameId, 5);
+    move(jarPlayerTwo, gameId, 6);
+    move(jarPlayerOne, gameId, 7);
+    move(jarPlayerTwo, gameId, 1);
+}
+
+function open(jar) {
+    let url = `${baseUrl}/api/connect-four/games/open`;
+    return http.post(url, {}, {jar, headers}).json().gameId;
+}
+
+function join(jar, gameId) {
+    let url = `${baseUrl}/api/connect-four/games/${gameId}/join`;
+    return http.post(url, {}, {jar, headers});
+}
+
+function move(jar, gameId, column) {
+    let url = `${baseUrl}/api/connect-four/games/${gameId}/move`;
+    return http.post(url, {column}, {jar, headers});
+}
