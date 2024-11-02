c407da54730ffa22470f8db848d6241cdf76232d
Markus Reinhold <markusreinhold@icloud.com>
2024-11-02 22:42:09 +0100

    Dismiss confirmation when clicked outside

--- assets/js/Common/ConfirmationButton.js
+++ assets/js/Common/ConfirmationButton.js
@@ -3,21 +3,26 @@ import {html} from 'uhtml/node.js'
 customElements.define('confirmation-button', class extends HTMLElement {
     connectedCallback() {
         this._initialChildren = Array.from(this.children);
-
         this._button = this.querySelector('button');
+        this._onDocumentClickHandler = this._onDocumentClick.bind(this);
+
         this._button?.addEventListener('click', this._onButtonClick.bind(this));
     }
 
     reset() {
+        document.removeEventListener('click', this._onDocumentClickHandler);
+        this.replaceChildren(...this._initialChildren);
         this._button?.removeAttribute('disabled');
         this._button?.classList.remove('btn-loading');
     }
 
-    _onButtonClick(event) {
+    _onButtonClick(e) {
+        setTimeout(() => document.addEventListener('click', this._onDocumentClickHandler), 0);
+
         this.replaceChildren(html`
             <div class="row row-gap-2">
                 <div class="col-6">
-                    <button @click="${() => this.replaceChildren(...this._initialChildren)}"
+                    <button @click="${this.reset.bind(this)}"
                             class="btn btn-outline-danger btn-icon w-100">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -44,10 +49,15 @@ customElements.define('confirmation-button', class extends HTMLElement {
     }
 
     _onYesClick(e) {
+        document.removeEventListener('click', this._onDocumentClickHandler);
         this.replaceChildren(...this._initialChildren);
         this._button?.setAttribute('disabled', '');
         this._button?.classList.add('btn-loading');
 
         this.dispatchEvent(new CustomEvent('confirmation-button:yes'));
+    }
+
+    _onDocumentClick(e) {
+        !this.contains(e.target) && this.reset();
     }
 });
