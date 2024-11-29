marein/php-gaming-website

commit 91fcf74d6406c1fc1fd2dd6adc6360b6124c7f5d

Use a design system (#232)
Add tabler.io, which contains a variety of components that
are useful for the project (showing player statistics in charts,
the chat widget, forms, grids, tables) #190.

Additional changes:
* Enable dark mode based on the preferred color scheme.
* All self-built components use the prefix gp-.
* Fix some ux related issues in the lobby.
* Use uhtml to render nodes instead of using the DOM api.
* Use default timeouts in acceptance tests as they
sometimes failed.

assets/js/ConnectFour/Game.js
@@ -1,9 +1,6 @@
 import {service} from './GameService.js'
 import {Game as GameModel} from './Model/Game.js'
-
-/**
- * todo: Render the game within <canvas>, see https://github.com/marein/php-gaming-website/issues/19.
- */
+import {html} from 'uhtml/node.js'
 
 customElements.define('connect-four-game', class extends HTMLElement {
     connectedCallback() {
@@ -11,49 +8,25 @@ customElements.define('connect-four-game', class extends HTMLElement {
 
         let game = JSON.parse(this.getAttribute('game'));
 
-        this._gameHolder = document.createElement('div');
-        this._gameHolder.classList.add('box');
+        this.append(this._gameNode = html`
+            <table class="gp-game user-select-none">
+                <tbody>${[...Array(game.height).keys()].map(y => y + 1).map(y => html`
+                    <tr>${[...Array(game.width).keys()].map(x => x + 1).map(x => html`
+                        <td class="gp-game__field"
+                            data-column="${x}"
+                            data-point="${x + ' ' + y}"></td>`)}
+                    </tr>`)}
+                </tbody>
+            </table>
+        `);
 
-        let table = document.createElement('table');
-        table.classList.add('game');
-
-        let tbody = document.createElement('tbody');
-
-        for (let y = 1; y <= game.height; y++) {
-            let tr = document.createElement('tr');
-
-            for (let x = 1; x <= game.width; x++) {
-                let td = document.createElement('td');
-                td.classList.add('game__field');
-                td.setAttribute('data-column', x.toString());
-                td.setAttribute('data-point', x.toString() + ' ' + y.toString());
-
-                tr.append(td);
-            }
-
-            tbody.append(tr);
-        }
-
-        table.append(tbody);
-        this._gameHolder.append(table);
-        this.append(this._gameHolder);
-
-        this._previousMoveButton = document.querySelector(
-            this.getAttribute('previous-move-selector')
-        );
-        this._nextMoveButton = document.querySelector(
-            this.getAttribute('next-move-selector')
-        );
-        this._followMovesButton = document.querySelector(
-            this.getAttribute('follow-moves-selector')
-        );
+        this._previousMoveButton = document.querySelector(this.getAttribute('previous-move-selector'));
+        this._nextMoveButton = document.querySelector(this.getAttribute('next-move-selector'));
+        this._followMovesButton = document.querySelector(this.getAttribute('follow-moves-selector'));
         this._game = GameModel.fromObject(game);
         this._numberOfCurrentMoveInView = this._game.numberOfMoves();
-        this._fields = this._gameHolder.querySelectorAll('.game__field');
-        this._colorToClass = {
-            1: 'game__field--red',
-            2: 'game__field--yellow'
-        };
+        this._fields = this._gameNode.querySelectorAll('.gp-game__field');
+        this._colorToClass = {1: 'bg-red', 2: 'bg-yellow'};
 
         this._showMovesUpTo(this._numberOfCurrentMoveInView);
 
@@ -68,12 +41,7 @@ customElements.define('connect-four-game', class extends HTMLElement {
      * @param {Number} index
      */
     _showMovesUpTo(index) {
-        // Clear fields
-        this._fields.forEach((field) => {
-            field.classList.remove('game__field--flash');
-            field.classList.remove('game__field--red');
-            field.classList.remove('game__field--yellow');
-        });
+        this._fields.forEach(field => field.classList.remove('gp-heartbeat', 'bg-red', 'bg-yellow'));
 
         this._game.moves.slice(0, index).forEach(this._showMove.bind(this));
         this._updateNavigationButtons();
@@ -85,15 +53,11 @@ customElements.define('connect-four-game', class extends HTMLElement {
      * @param {{x:Number, y:Number, color:Number}} move
      */
     _showMove(move) {
-        let field = this._gameHolder.querySelector('.game__field[data-point="' + move.x + ' ' + move.y + '"]');
-        field.classList.add(
-            this._colorToClass[move.color]
-        );
+        let field = this._gameNode.querySelector(`.gp-game__field[data-point="${move.x} ${move.y}"]`);
+        field.classList.add(this._colorToClass[move.color]);
 
-        this._fields.forEach((field) => {
-            field.classList.remove('game__field--flash');
-        });
-        field.classList.add('game__field--flash');
+        this._fields.forEach(field => field.classList.remove('gp-heartbeat'));
+        field.classList.add('gp-heartbeat');
     }
 
     /**
@@ -102,20 +66,14 @@ customElements.define('connect-four-game', class extends HTMLElement {
      */
     _updateNavigationButtons() {
         let isCurrentMoveTheLastMove = this._numberOfCurrentMoveInView === this._game.numberOfMoves();
-        let isCurrentMoveBeforeTheFirstMove = this._numberOfCurrentMoveInView === 0;
 
+        this._previousMoveButton.disabled = this._numberOfCurrentMoveInView === 0;
         this._nextMoveButton.disabled = isCurrentMoveTheLastMove;
         this._followMovesButton.disabled = isCurrentMoveTheLastMove;
-        this._previousMoveButton.disabled = isCurrentMoveBeforeTheFirstMove;
-
-        this._nextMoveButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
-        this._followMovesButton.classList.toggle('disabled', isCurrentMoveTheLastMove);
-        this._previousMoveButton.classList.toggle('disabled', isCurrentMoveBeforeTheFirstMove);
 
         // Remove flashing if the user follow the moves.
         if (isCurrentMoveTheLastMove) {
-            this._followMovesButton.classList.remove('button--yellow');
-            this._followMovesButton.classList.remove('button--flash');
+            this._followMovesButton.classList.remove('btn-warning', 'gp-heartbeat');
         }
     }
 
@@ -125,39 +83,31 @@ customElements.define('connect-four-game', class extends HTMLElement {
      * @param {{x:Number, y:Number, color:Number}} move
      */
     _onMoveAppendedToGame(move) {
-        // Only show if the the user follow the moves. Otherwise notify user that a new move is available.
+        // Only show if the user follow the moves. Otherwise notify user that a new move is available.
         if (this._followMovesButton.disabled === true) {
             this._showMove(move);
             this._numberOfCurrentMoveInView++;
             this._updateNavigationButtons();
         } else {
-            this._followMovesButton.classList.add('button--yellow');
-            this._followMovesButton.classList.add('button--flash');
+            this._followMovesButton.classList.add('btn-warning', 'gp-heartbeat');
         }
     }
 
     _onFieldClick(event) {
         let cell = event.target;
 
-        this._gameHolder.classList.add('loading-indicator');
+        let loadingTimeout = setTimeout(() => this._gameNode.classList.add('gp-loading'), 250);
 
-        service.move(
-            this._game.gameId,
-            cell.dataset.column
-        ).then(() => {
-            this._gameHolder.classList.remove('loading-indicator');
-        }).catch(() => {
-            // todo: Handle exception based on error
-            this._gameHolder.classList.remove('loading-indicator');
-        });
+        service.move(this._game.gameId, cell.dataset.column)
+            .catch(() => true)
+            .finally(() => {
+                if (loadingTimeout) clearTimeout(loadingTimeout);
+                this._gameNode.classList.remove('gp-loading');
+            });
     }
 
     _onPlayerMoved(event) {
-        this._game.appendMove({
-            x: event.detail.x,
-            y: event.detail.y,
-            color: event.detail.color,
-        });
+        this._game.appendMove(event.detail);
     }
 
     _onPreviousMoveClick(event) {
@@ -189,7 +139,7 @@ customElements.define('connect-four-game', class extends HTMLElement {
             this._onDisconnect.push(() => window.removeEventListener(n, f));
         })('ConnectFour.PlayerMoved', this._onPlayerMoved.bind(this));
 
-        this._fields.forEach((field) => {
+        this._fields.forEach(field => {
             field.addEventListener('click', this._onFieldClick.bind(this));
         });
 

assets/js/ConnectFour/Model/Game.js
@@ -27,9 +27,7 @@ export class Game {
         if (!this.hasMove(move)) {
             this.moves.push(move);
 
-            this.onMoveAppendedObservers.forEach((callback) => {
-                callback(move);
-            });
+            this.onMoveAppendedObservers.forEach(callback => callback(move));
         }
     }
 

.dockerignore
@@ -1,3 +1,4 @@
+/assets/vendor
 /var
 /vendor
 /public/assets

.gitignore
@@ -1,3 +1,4 @@
+/assets/vendor
 /codeception.yml
 /phpcs.xml
 /phpstan.neon

assets/css/app.css
@@ -1,286 +1,90 @@
-/**
- * Don't look at this. CSS is not my domain.
- * Feel free to contribute and structure properly :).
- */
-@keyframes flash {
-    0%, 100% {
-        filter: brightness(0.9);
-    }
-    20% {
-        filter: brightness(1.1);
-    }
-}
-
-body, ul, p, a, button {
-    margin: 0;
-    padding: 0;
-    color: rgb(173, 173, 173);
-    font-family: "Verdana";
-    font-size: 15px;
-}
-
-body {
-    background-color: rgb(70, 100, 100);
-}
-
-button {
-    border: none;
-}
-
-button:hover {
-    cursor: pointer;
-}
-
-.button {
-    text-decoration: none;
-    border: none;
-    padding: 15px 15px;
-    color: rgb(173, 173, 173);
-    background-color: rgb(32, 32, 32);
-    font-size: 15px;
-    display: inline-block;
-}
-
-.button:hover {
-    cursor: pointer;
-}
-
-.button--yellow {
-    background-color: rgb(171, 171, 105);
-    color: rgb(230, 230, 230);
-}
-
-.button--flash {
-    animation: flash 1s infinite;
-}
-
-.left, .middle, .right {
-    float: none;
-    width: 100%;
-}
-
-.main-navigation {
-    margin: 15px 15px 15px 15px;
-    color: rgb(0, 0, 0);
-}
-
-.main-navigation a {
-    color: rgb(0, 0, 0);
-}
-
-.box {
-    margin: 0 15px 15px 15px;
-    padding: 10px;
-    background-color: rgb(46, 48, 50);
-}
-
-.notification {
-    z-index: 100;
-    position: fixed;
-    right: 15px;
-    list-style-type: none;
-}
-
-.notification__message {
-    width: 200px;
-    max-width: 100%;
-    min-height: 20px;
-    margin-bottom: 15px;
-    padding: 15px;
-    color: rgb(0, 0, 0);
-    background-color: rgb(200, 101, 85);
-    border: 1px solid rgb(200, 81, 85);
-    opacity: 0.9;
-    border-radius: 20px;
+.gp-page-progress {
+    z-index: 10000;
     top: 0;
-    position: relative;
+    left: 0;
+    position: fixed;
+    height: 0.2em;
+    width: 0;
+    background-color: var(--tblr-primary);
+    animation: gp-page-progress 10s infinite;
 }
 
-.notification__message--close {
-    transition: all 0.25s ease-in-out;
-    top: 25px;
+.gp-page-progress--finish {
+    width: 100%;
+    transition: opacity .25s ease .2s;
     opacity: 0;
 }
 
-chat-widget {
-    display: block;
-    margin: 0 15px 15px 15px;
-    padding: 10px;
-    background-color: rgb(46, 48, 50);
-    color: rgb(173, 173, 173);
+@keyframes gp-page-progress {
+    to {
+        width: 100%;
+    }
 }
 
-.chat__messages {
-    list-style: none;
-    margin-bottom: 15px;
-    height: 300px;
-    overflow: scroll;
-    overflow-wrap: break-word;
+.gp-heartbeat {
+    animation: gp-heartbeat 1s infinite;
 }
 
-.chat__messages__message {
-    margin-bottom: 5px;
-    font-size: 14px;
+@keyframes gp-heartbeat {
+    0%, 100% {
+        filter: brightness(0.95);
+    }
+    20% {
+        filter: brightness(1.15);
+    }
 }
 
-.chat__input {
+.gp-loading {
+    filter: brightness(0.85);
+    cursor: wait !important;
+}
+
+.gp-loading * {
+    cursor: wait !important;
+}
+
+.gp-fadeout {
+    transition: all 0.25s ease-in-out;
+    opacity: 0;
+}
+
+notification-list {
+    width: 325px;
+    position: fixed;
+    top: 15px;
+    right: 15px;
+    z-index: 10000;
+}
+
+.gp-game {
     width: 100%;
-    max-width: 100%;
-    height: 50px;
-    max-height: 50px;
-    color: rgb(173, 173, 173);
-    background-color: rgb(32, 32, 32);
-    border: 3px solid rgb(50, 50, 50);
+    background-color: var(--tblr-blue);
+    border: var(--tblr-border-width) solid var(--tblr-border-color-translucent);
+    border-radius: var(--tblr-border-radius);
+    border-collapse: initial;
+    border-spacing: .5rem;
+    padding: .25rem .25rem;
 }
 
-.chat__messages__message span {
-    display: block;
-    font-size: 12px;
-    margin-bottom: 3px;
-}
-
-.game-list {
-    list-style: none;
-    height: 400px;
-    margin: 0 15px 15px 15px;
-    background-color: rgb(46, 48, 50);
-}
-
-.game-list__game {
-    height: 40px;
-}
-
-.game-list__game button {
-    display: block;
-    height: 100%;
-    width: 100%;
-    text-align: left;
-    background-color: rgb(46, 48, 50);
-}
-
-.game-list__game button span {
-    margin-left: 30px;
-}
-
-.game-list__game--user-game button {
-    background-color: rgb(85, 131, 131);
-}
-
-.game-list__game--remove-soon button {
-    background-color: rgb(37, 38, 40);
-}
-
-.game {
-    width: 100%;
-}
-
-.game__field {
-    background-color: rgb(32, 32, 32);
-    border: 3px solid rgb(50, 50, 50);
+.gp-game__field {
+    border: var(--tblr-border-width) solid #323232;
     border-radius: 50%;
     cursor: pointer;
+    background-color: var(--tblr-body-bg);
 }
 
-.game__field--red {
-    background-color: rgb(171, 111, 85);
-}
-
-.game__field--yellow {
-    background-color: rgb(171, 171, 105);
-}
-
-.game__field--flash {
-    animation: flash 1s infinite;
-}
-
-.loading-indicator .game__field--flash {
-    animation: paused;
-}
-
-.game__field::before {
+.gp-game__field::before {
     display: block;
     content: "";
     padding-top: 100%;
     position: relative;
 }
 
-.loading-indicator {
-    z-index: 0;
-    position: relative
+[data-bs-theme=dark] .gp-game {
+    background-color: var(--tblr-bg-surface);
+    border-color: var(--tblr-blue);
 }
 
-.loading-indicator:before {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    content: "";
-    display: block;
-    background-color: rgba(255, 255, 255, .3);
-    background-image: url("../img/loading-indicator.svg");
-    background-position: center;
-    background-repeat: no-repeat;
-}
-
-.disabled {
-    z-index: 0;
-    position: relative
-}
-
-.disabled:before {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    content: "";
-    display: block;
-    background-color: rgba(255, 255, 255, .3);
-}
-
-:not(:defined):not(notification-list) {
-    display: inline-block;
-    min-height: 1px;
-}
-
-.progress {
-    top: 0;
-    left: 0;
-    position: fixed;
-    height: 3px;
-    width: 0;
-    background-color: #0078ff;
-    animation: progress 10s infinite;
-}
-
-.progress--finish {
-    width: 100%;
-    transition: opacity .25s ease .2s;
-    opacity: 0;
-}
-
-@keyframes progress {
-    to { width: 100%; }
-}
-
-@media screen and (min-width: 960px) {
-    .left {
-        width: 25%;
-        float: left;
-    }
-
-    .middle {
-        width: 50%;
-        float: left;
-    }
-
-    .right {
-        width: 25%;
-        float: left;
-    }
-
-    .game-list {
-        margin: 0;
-    }
+[data-bs-theme=dark] .gp-game__field {
+    border-color: var(--tblr-blue);
 }

assets/img/loading-indicator.svg
@@ -1,33 +1,0 @@
-<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
-<svg width="120" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-    <circle cx="15" cy="15" r="15">
-        <animate attributeName="r" from="15" to="15"
-                 begin="0s" dur="0.8s"
-                 values="15;9;15" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="1" to="1"
-                 begin="0s" dur="0.8s"
-                 values="1;.5;1" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-    <circle cx="60" cy="15" r="9" fill-opacity="0.3">
-        <animate attributeName="r" from="9" to="9"
-                 begin="0s" dur="0.8s"
-                 values="9;15;9" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="0.5" to="0.5"
-                 begin="0s" dur="0.8s"
-                 values=".5;1;.5" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-    <circle cx="105" cy="15" r="15">
-        <animate attributeName="r" from="15" to="15"
-                 begin="0s" dur="0.8s"
-                 values="15;9;15" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="1" to="1"
-                 begin="0s" dur="0.8s"
-                 values="1;.5;1" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-</svg>

assets/js/Chat/Widget.js
@@ -1,29 +1,38 @@
 import {service} from './ChatService.js'
+import {html} from 'uhtml/node.js'
 
 customElements.define('chat-widget', class extends HTMLElement {
     connectedCallback() {
         this._onDisconnect = [];
 
-        this.classList.add('loading-indicator');
-
-        this._messageHolder = document.createElement('ul');
-        this._messageHolder.classList.add('chat__messages');
-
-        this._input = document.createElement('textarea');
-        this._input.classList.add('chat__input');
-        this._input.setAttribute('type', 'text');
-        this._input.setAttribute('name', 'message');
-
-        this.append(this._messageHolder);
-        this.append(this._input);
+        this.append(this._rootElement = html`
+            <div class="card gp-loading" style="height: 400px" id="chat">
+                <div class="card-body scrollable">
+                    <div class="chat">
+                        <div class="chat-bubbles">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <input type="text"
+                           name="message"
+                           class="form-control"
+                           autocomplete="off"
+                           placeholder="Type message">
+                </div>
+            </div>
+        `);
 
         this._chatId = '';
+        this._authorId = this.getAttribute('author-id');
+        this._input = this.querySelector('[name="message"]');
+        this._messageHolder = this.querySelector('.chat-bubbles');
+        this._scrollElement = this.querySelector('.card-body');
         this._messageBuffer = [];
         this._isAlreadyInitialized = false;
         this._secondsBeforeRetryAfterLoadMessageFailure = parseInt(this.getAttribute('seconds-before-retry'));
 
-        let chatId = this.getAttribute('chat-id');
-
+        const chatId = this.getAttribute('chat-id');
         if (chatId) {
             this._initialize(chatId);
         }
@@ -50,22 +59,22 @@ customElements.define('chat-widget', class extends HTMLElement {
      * @param {String} chatId
      */
     _loadMessages(chatId) {
-        service.messages(chatId).then((messages) => {
-            messages.messages.forEach((message) => {
-                this._appendMessage(message);
+        service.messages(chatId)
+            .then((messages) => {
+                messages.messages.forEach(message => this._appendMessage(message));
+
+                this._isAlreadyInitialized = true;
+
+                this._flushMessageBuffer();
+
+                this._rootElement.classList.remove('gp-loading');
+            })
+            .catch((e) => {
+                // Automatic retry after x seconds.
+                setTimeout(() => {
+                    this._loadMessages(chatId);
+                }, this._secondsBeforeRetryAfterLoadMessageFailure * 1000);
             });
-
-            this._isAlreadyInitialized = true;
-
-            this._flushMessageBuffer();
-
-            this.classList.remove('loading-indicator');
-        }).catch(() => {
-            // Automatic retry after x seconds.
-            setTimeout(() => {
-                this._loadMessages(chatId);
-            }, this._secondsBeforeRetryAfterLoadMessageFailure * 1000);
-        });
     }
 
     /**
@@ -77,7 +86,7 @@ customElements.define('chat-widget', class extends HTMLElement {
                 this._createMessageNode(message)
             );
 
-            this._messageHolder.scrollTop = this._messageHolder.scrollHeight;
+            this._scrollElement.scrollTop = this._scrollElement.scrollHeight;
         }
     }
 
@@ -102,24 +111,30 @@ customElements.define('chat-widget', class extends HTMLElement {
      * @returns {Node}
      */
     _createMessageNode(message) {
-        let writtenAt = new Date(message.writtenAt);
-        let hours = ('0' + writtenAt.getHours()).slice(-2);
-        let minutes = ('0' + writtenAt.getMinutes()).slice(-2);
+        const writtenAt = new Date(message.writtenAt);
+        const hours = ('0' + writtenAt.getHours()).slice(-2);
+        const minutes = ('0' + writtenAt.getMinutes()).slice(-2);
+        const isSameAuthor = this._authorId === message.authorId;
 
-        let author = 'Anonymous';
-
-        let span = document.createElement('span');
-        span.innerText = hours + ':' + minutes + ' - ' + author;
-
-        let text = document.createTextNode(message.message);
-
-        let li = document.createElement('li');
-        li.dataset.id = message.messageId;
-        li.classList.add('chat__messages__message');
-        li.append(span);
-        li.append(text);
-
-        return li;
+        return html`
+            <div class="chat-item" data-id="${message.messageId}">
+                <div class="${`row${isSameAuthor ? ' align-items-end justify-content-end' : ''}`}">
+                    <div class="col-11">
+                        <div class="${`chat-bubble${isSameAuthor ? ' chat-bubble-me' : ''}`}">
+                            <div class="chat-bubble-title">
+                                <div class="row">
+                                    <div class="col chat-bubble-author">${'Anonymous'}</div>
+                                    <div class="col-auto chat-bubble-date">${hours + ':' + minutes}</div>
+                                </div>
+                            </div>
+                            <div class="chat-bubble-body">
+                                <p>${message.message}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
     }
 
     _clearInput() {
@@ -127,18 +142,15 @@ customElements.define('chat-widget', class extends HTMLElement {
     }
 
     _onKeyPress(event) {
-        if (event.which === 13 && !event.shiftKey) {
-            event.preventDefault();
-            let message = this._input.value;
+        if (event.which !== 13) return;
 
-            this._clearInput();
+        event.preventDefault();
+        let message = this._input.value;
 
-            if (message.trim() !== '') {
-                service.writeMessage(
-                    this._chatId,
-                    message
-                );
-            }
+        this._clearInput();
+
+        if (message.trim() !== '') {
+            service.writeMessage(this._chatId, message);
         }
     }
 

commit 221bde8d5578836e66d0ce94852952da86123b4e

Fix bug that applies moves twice
The bug was introduced in #232.

assets/js/ConnectFour/Game.js
@@ -122,7 +122,11 @@ customElements.define('connect-four-game', class extends HTMLElement {
     }
 
     _onPlayerMoved(event) {
-        this._game.appendMove(event.detail);
+        this._game.appendMove({
+            x: event.detail.x,
+            y: event.detail.y,
+            color: event.detail.color,
+        });
     }
 
     _onGameWon(event) {