f1288d0e6dd31ae9e79ffcef1bf4b27f10d51224
Markus Reinhold <markusreinhold@icloud.com>
2025-02-19 20:45:35 +0100

    Remove temporary tryUser() method

--- src/WebInterface/Infrastructure/Security/Security.php
+++ src/WebInterface/Infrastructure/Security/Security.php
@@ -7,7 +7,6 @@ namespace Gaming\WebInterface\Infrastructure\Security;
 use Gaming\Common\Bus\Bus;
 use Gaming\Identity\Application\User\Command\ArriveCommand;
 use Symfony\Bundle\SecurityBundle\Security as SymfonySecurity;
-use Symfony\Component\Uid\NilUuid;
 
 final class Security
 {
@@ -17,21 +16,10 @@ final class Security
     ) {
     }
 
-    public function tryUser(): User
-    {
-        $user = $this->security->getUser();
-
-        return match (true) {
-            $user instanceof User => $user,
-            default => new User((new NilUuid())->toRfc4122())
-        };
-    }
-
     public function getUser(): User
     {
-        $user = $this->tryUser();
-        if ((new NilUuid())->toRfc4122() !== $user->getUserIdentifier()) {
-            return $user;
+        if ($this->security->getUser() instanceof User) {
+            return $this->security->getUser();
         }
 
         $this->security->login(
--- src/WebInterface/Presentation/Http/PageController.php
+++ src/WebInterface/Presentation/Http/PageController.php
@@ -9,17 +9,17 @@ use Gaming\ConnectFour\Application\Game\Query\GameQuery;
 use Gaming\ConnectFour\Application\Game\Query\GamesByPlayerQuery;
 use Gaming\ConnectFour\Application\Game\Query\OpenGamesQuery;
 use Gaming\ConnectFour\Application\Game\Query\RunningGamesQuery;
-use Gaming\WebInterface\Infrastructure\Security\Security;
+use Gaming\WebInterface\Infrastructure\Security\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Twig\Environment;
 
 final class PageController
 {
     public function __construct(
         private readonly Environment $twig,
-        private readonly Bus $connectFourQueryBus,
-        private readonly Security $security
+        private readonly Bus $connectFourQueryBus
     ) {
     }
 
@@ -29,8 +29,7 @@ final class PageController
             $this->twig->render('@web-interface/lobby.html.twig', [
                 'maximumNumberOfGamesInList' => 10,
                 'openGames' => $this->connectFourQueryBus->handle(new OpenGamesQuery()),
-                'runningGames' => $this->connectFourQueryBus->handle(new RunningGamesQuery()),
-                'user' => $this->security->tryUser()
+                'runningGames' => $this->connectFourQueryBus->handle(new RunningGamesQuery())
             ])
         );
     }
@@ -39,19 +38,18 @@ final class PageController
     {
         return new Response(
             $this->twig->render('@web-interface/game.html.twig', [
-                'game' => $this->connectFourQueryBus->handle(new GameQuery($id)),
-                'user' => $this->security->tryUser()
+                'game' => $this->connectFourQueryBus->handle(new GameQuery($id))
             ])
         );
     }
 
-    public function profileAction(Request $request): Response
+    public function profileAction(#[CurrentUser] ?User $user, Request $request): Response
     {
         return new Response(
             $this->twig->render('@web-interface/profile.html.twig', [
-                'games' => $this->connectFourQueryBus->handle(
-                    new GamesByPlayerQuery($this->security->tryUser()->getUserIdentifier())
-                )->games()
+                'games' => $user !== null
+                    ? $this->connectFourQueryBus->handle(new GamesByPlayerQuery($user->getUserIdentifier()))->games()
+                    : []
             ])
         );
     }
