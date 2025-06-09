184cc53da8c3396d1bd0b8836cd0600edf817adc
Markus Reinhold <markusreinhold@icloud.com>
2025-06-09 15:33:32 +0200

    Retry for fetching server time

--- assets/js/Common/ServerTime.js
+++ assets/js/Common/ServerTime.js
@@ -1,20 +1,39 @@
 const globalConfig = document.querySelector('meta[name="server-time-config"]');
 const url = globalConfig?.getAttribute('data-url') || '/api/server-time';
+const defaultMaxJitter = parseInt(globalConfig?.getAttribute('data-max-jitter') || 1000);
+const defaultDelay = parseInt(globalConfig?.getAttribute('data-delay') || 100);
 let offsetMs = 0;
 
 sync();
 
-export async function sync() {
-    const requestTime = Date.now();
-    const response = await fetch(url);
-    const serverTime = await response.json();
-    const roundTripTime = Date.now() - requestTime;
-    const timeWhenServerCreatedServerTime = requestTime + roundTripTime / 2;
-    offsetMs = serverTime - timeWhenServerCreatedServerTime;
+/**
+ * @param {Number} retries
+ * @param {Number} delay
+ * @param {Number} maxJitter
+ */
+export async function sync(retries = 5, delay = defaultDelay, maxJitter = defaultMaxJitter) {
+    try {
+        const requestTime = Date.now();
+        const response = await fetch(url);
+        if (!response.ok) throw new Error();
+        const serverTime = await response.json();
+        const roundTripTime = Date.now() - requestTime;
+        const timeWhenServerCreatedServerTime = requestTime + roundTripTime / 2;
+        offsetMs = serverTime - timeWhenServerCreatedServerTime;
+        window.dispatchEvent(new CustomEvent('server-time:sync', {detail: { offsetMs }}));
+    } catch (e) {
+        if (retries <= 0) {
+            window.dispatchEvent(new CustomEvent('server-time:sync-error'));
+            return;
+        }
+
+        await new Promise(r => setTimeout(r, delay + Math.floor(Math.random() * maxJitter)));
+        await sync(retries - 1, delay * 2);
+    }
 }
 
 /**
- * @returns {int}
+ * @returns {Number}
  */
 export function now() {
     return Date.now() + offsetMs;
