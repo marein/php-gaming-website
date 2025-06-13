06a2bb3023bb2212104a9b3b7d7c8056201c51ad
Markus Reinhold <markusreinhold@icloud.com>
2025-06-13 22:24:22 +0200

    Use default timers

--- src/Common/Timer/TimerFactory.php
+++ src/Common/Timer/TimerFactory.php
@@ -2,26 +2,17 @@
 
 namespace Gaming\Common\Timer;
 
-use InvalidArgumentException;
-
 final class TimerFactory
 {
-    /**
-     * @throws InvalidArgumentException
-     */
-    public static function fromString(string $config): Timer
+    public static function fromString(string $config): ?Timer
     {
         $parts = explode(':', $config);
         $type = array_shift($parts);
 
         return match ($type) {
-            'move' => count($parts) === 1
-                ? MoveTimer::set((int)$parts[0])
-                : throw new InvalidArgumentException('Format: move:<msPerMove>'),
-            'game' => count($parts) === 2
-                ? GameTimer::set((int)$parts[0], (int)$parts[1])
-                : throw new InvalidArgumentException('Format: game:<baseMs>:<incrementMs>'),
-            default => throw new InvalidArgumentException('Unknown timer: ' . $type),
+            'move' => count($parts) === 1 ? MoveTimer::set((int)$parts[0]) : MoveTimer::set(15000),
+            'game' => count($parts) === 2 ? GameTimer::set((int)$parts[0], (int)$parts[1]) : GameTimer::set(60000, 0),
+            default => null
         };
     }
 }
