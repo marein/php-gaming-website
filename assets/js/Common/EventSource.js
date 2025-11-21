3fe9559e1aa5a2bcc12cfc78cf448e71c1b726c0
Markus Reinhold <markusreinhold@icloud.com>
2025-11-21 18:36:00 +0100

    Revert EventSource.js
    Buffering on the client causes more trouble
    than it's worth.

--- assets/js/Common/EventSource.js
+++ assets/js/Common/EventSource.js
@@ -1,8 +1,5 @@
 /**
- * @typedef {{[key: number]: Subscription}} Subscriptions
- * @typedef {{channel: string, listeners: Listeners}} Subscription
  * @typedef {{[key: string]: Function}} Listeners
- * @typedef {{type: string, channels: string[], payload: string}} Message
  */
 
 const eventTarget = new EventTarget();
@@ -12,26 +9,17 @@ const subscriptions = {};
 let eventSource = null;
 const baseUrl = globalConfig?.getAttribute('data-base-url') || '/sse/sub?id=';
 let debounceTimeout = null;
-const globalDebounceTimeoutMs = parseInt(globalConfig?.getAttribute('data-debounce-ms') ?? 150);
-let activeChannels = [];
-let messageBuffer = [];
-const messageBufferSize = parseInt(globalConfig?.getAttribute('data-message-buffer-size') ?? 100);
-let messageTimeoutMs = parseInt(globalConfig?.getAttribute('data-message-timeout-ms') ?? 10000);
-
-setInterval(() => {
-    const now = Date.now();
-    messageBuffer = messageBuffer.filter(m => m.removeAfter > now);
-}, 1000);
+const globalDebounceTimeoutMs = globalConfig?.getAttribute('data-debounce-ms') ?? 150;
 
 function connect(debounceTimeoutMs = null) {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
         if (eventSource) eventSource.close();
 
-        activeChannels = [...new Set(Object.values(subscriptions).map(s => s.channel))];
-        if (activeChannels.length === 0) return;
+        const uniqueChannels = [...new Set(Object.values(subscriptions).map(s => s.channel))];
+        if (uniqueChannels.length === 0) return;
 
-        eventSource = new EventSource(baseUrl + activeChannels.join(','));
+        eventSource = new EventSource(baseUrl + uniqueChannels.join(','));
         eventSource.onmessage = onMessage;
         eventSource.onopen = onOpen;
         eventSource.onerror = onError;
@@ -40,15 +28,12 @@ function connect(debounceTimeoutMs = null) {
 
 function onMessage(event) {
     let [, type, channels, payload] = event.data.match(/([^:]+):([^:]+):(.*)/);
-    channels = channels.split(',').filter(c => activeChannels.indexOf(c) !== -1);
+    channels = channels.split(',');
 
-    const message = {type, channels, payload, removeAfter: Date.now() + messageTimeoutMs};
-    messageBuffer.push(message);
-    if (messageBuffer.length > messageBufferSize) {
-        messageBuffer.shift();
-    }
-
-    publish(subscriptions, [message]);
+    Object.values(subscriptions).forEach(s => {
+        if (channels.indexOf(s.channel) === -1) return;
+        s.listeners[type]?.({type, detail: JSON.parse(payload)})
+    });
 }
 
 function onOpen() {
@@ -64,25 +49,11 @@ function onError () {
 }
 
 /**
- * @param {Subscriptions} subscriptions
- * @param {Message[]} messages
- */
-function publish(subscriptions, messages) {
-    Object.values(subscriptions).forEach(s => {
-        messages.forEach(m => {
-            if (m.channels.indexOf(s.channel) === -1) return;
-            s.listeners[m.type]?.({type: m.type, detail: JSON.parse(m.payload)})
-        });
-    });
-}
-
-/**
  * @param {String} channel
  * @param {Listeners} listeners
  * @param {AbortSignal|null} signal
  */
 export function subscribe(channel, listeners, signal = null) {
-    const doesChannelExist = Object.values(subscriptions).some(s => s.channel === channel);
     const subscriptionId = ++currentSubscriptionId;
     subscriptions[subscriptionId] = {channel, listeners};
     signal?.addEventListener('abort', () => {
@@ -90,8 +61,7 @@ export function subscribe(channel, listeners, signal = null) {
         const doesChannelExist = Object.values(subscriptions).some(s => s.channel === channel);
         !doesChannelExist && connect();
     });
-    doesChannelExist && publish([subscriptions[subscriptionId]], messageBuffer);
-    !doesChannelExist && connect();
+    connect();
 }
 
 export const addEventListener = (...args) => eventTarget.addEventListener(...args);
